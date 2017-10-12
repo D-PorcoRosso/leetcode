c9d9a6bccd2b444a5f64f91ff5e2967d57fec2dc
Daniel <shechiway@gmail.com>
2017-10-13 00:22:41 +0800

    [Q58][38.15%][v2]

--- 58/Solution.java
+++ 58/Solution.java
@@ -22,11 +22,25 @@ class Solution {
         return length;
     }
 
+    public int lengthOfLastWord2(String s) {
+        int length = 0;
+        String[] splitResult = s.split(" ");
+        if ( splitResult.length == 0 )
+            return length;
+        String last = splitResult[splitResult.length-1];
+        return last.length();
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
     
         System.out.println(solution.lengthOfLastWord("Hello World"));
         System.out.println(solution.lengthOfLastWord("World "));
         System.out.println(solution.lengthOfLastWord("b   a    "));
+        System.out.println(solution.lengthOfLastWord(" "));
+        System.out.println(solution.lengthOfLastWord2("Hello World"));
+        System.out.println(solution.lengthOfLastWord2("World "));
+        System.out.println(solution.lengthOfLastWord2("b   a    "));
+        System.out.println(solution.lengthOfLastWord2(" "));
     }
 }
