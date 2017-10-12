74696c72afccd4dd269efe4c2b893caf3ccedea0
Daniel <shechiway@gmail.com>
2017-10-13 00:07:29 +0800

    [Q58][13.64%][v1]

---
+++ 58/Solution.java
@@ -0,0 +1,32 @@
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        boolean prepareClear = false;
+        for( int i = 0 ; i < s.length() ; i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case ' ':
+                    prepareClear = true;
+                    break;
+            
+                default:
+                    if ( prepareClear ) {
+                        length = 0;
+                        prepareClear = false;
+                    }
+                    length++;
+                    break;
+            }
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    
+        System.out.println(solution.lengthOfLastWord("Hello World"));
+        System.out.println(solution.lengthOfLastWord("World "));
+        System.out.println(solution.lengthOfLastWord("b   a    "));
+    }
+}
