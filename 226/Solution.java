58547327dde1b8bbd23bc83ae5d81e645e908532
Daniel <shechiway@gmail.com>
2017-10-18 22:47:53 +0800

    [Q226][0.5%][v1] what!!!!!!!

---
+++ 226/Solution.java
@@ -0,0 +1,45 @@
+class Solution {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {val = x;}
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if ( root != null ) {
+            TreeNode temp = root.right;
+            root.right = root.left;
+            root.left = temp;
+            System.out.println(root.val);
+            invertTree(root.left);
+            invertTree(root.right);
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode one = new TreeNode(4);
+        TreeNode two = new TreeNode(2);
+        TreeNode three = new TreeNode(7);
+        TreeNode four = new TreeNode(1);
+        TreeNode a = new TreeNode(3);
+        TreeNode b = new TreeNode(6);
+        TreeNode c = new TreeNode(9);
+
+        one.left = two;
+        one.right = three;
+        two.left = four;
+        two.right = a;
+        three.left = b;
+        three.right = c;
+        
+        TreeNode result = solution.invertTree(one);
+    }
+
+    private void printNode(TreeNode node) {
+        System.out.println(node.val);
+    }
+}
