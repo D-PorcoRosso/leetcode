31173c9540eaae83ea6257a47f9c8ef3898c57b1
Daniel <shechiway@gmail.com>
2017-11-29 08:46:00 +0800

    [Q101][%][wrong] fail at             1            / \           2   2           \   /            3 3

--- 101/Solution.java
+++ 101/Solution.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
     static class TreeNode {
@@ -13,57 +14,64 @@ class Solution {
     }
 
     public boolean isSymmetric(TreeNode root) {
-        ArrayList<Integer> front = new ArrayList<>();
-        ArrayList<Integer> back = new ArrayList<>();
-        front(root,front);
-        back(root,back);
 
+        if ( root == null )
+            return true;
 
+        Queue<TreeNode> bfsQueue = new LinkedList<>();
+        ArrayList<Integer> flatNode = new ArrayList<>();
 
-        for ( int index = 0; index < front.size() ; index++ ) {
-            if ( front.get(index) != back.get(index) )
+        bfsQueue.offer(root);
+
+        while(!bfsQueue.isEmpty()) {
+            TreeNode node = bfsQueue.poll();
+            if ( node != null ) {
+                flatNode.add(node.val);
+                bfsQueue.offer(node.left);
+                bfsQueue.offer(node.right);
+            }
+        }
+
+        int power2 = 1, sum = 1, index = 0, power2SubIndex = 1;
+        for ( ; index < flatNode.size() ; index++ ) {
+            try {
+                if ( flatNode.get(index) == flatNode.get(power2SubIndex-1) ) {
+                    power2SubIndex--;
+                } else
+                    return false;
+            } catch ( IndexOutOfBoundsException e ) {
                 return false;
+            }
+            if ( index != flatNode.size() - 1) {
+                if ( index == sum - 1 ) {
+                    power2 *= 2;
+                    sum += power2;
+                    power2SubIndex = sum;
+                }
+            }
         }
+        if ( index != sum )
+            return false;
+
         return true;
-    }
-
-    private void front(TreeNode root, ArrayList<Integer> front) {
-        if ( root == null ) {
-            front.add(null);
-            return;
-        }
-
-        front(root.left, front);
-        front.add(root.val);
-        front(root.right, front);
-    }
-
-    private void back(TreeNode root, ArrayList<Integer> back) {
-        if (root == null) {
-            back.add(null);
-            return;
-        }
-        back(root.right, back);
-        back.add(root.val);
-        back(root.left, back);
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         TreeNode one = new TreeNode(1);
         TreeNode two = new TreeNode(2);
-        TreeNode three = new TreeNode(2);
+        TreeNode three = new TreeNode(3);
         TreeNode four = new TreeNode(3);
-        TreeNode a = new TreeNode(4);
-        TreeNode b = new TreeNode(4);
-        TreeNode c = new TreeNode(3);
+        //TreeNode a = new TreeNode(4);
+        TreeNode b = new TreeNode(2);
+        //TreeNode c = new TreeNode(3);
 
         one.left = two;
         one.right = three;
         two.left = four;
-        two.right = a;
+        //two.right = a;
         three.left = b;
-        three.right = c;
+        //three.right = c;
 
         System.out.println(solution.isSymmetric(one));
     }
