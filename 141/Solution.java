d2a60645a41c02ff7fc3077a2ac24bb1746037fc
Daniel <shechiway@gmail.com>
2017-11-30 08:10:36 +0800

    [Q141][0.12%][v1]

---
+++ 141/Solution.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+class Solution {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if ( head == null )
+            return false;
+        ArrayList<ListNode> nodes = new ArrayList<>();
+        boolean hasCycle = false;
+        do {
+            if ( nodes.contains(head) ) {
+                hasCycle = true;
+                break;
+            } else {
+                nodes.add(head);
+            }
+            head = head.next;
+        } while ( head != null );
+
+        return hasCycle;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(7);
+        ListNode four = new ListNode(1);
+
+        one.next = two;
+        two.next = three;
+        three.next = four;
+        four.next = null;
+        System.out.println(solution.hasCycle(one));
+
+        four.next = one;
+        System.out.println(solution.hasCycle(one));
+
+        four.next = two;
+        System.out.println(solution.hasCycle(one));
+    }
+}
