87e477169170167341907ecfae84d9f5e5d108a1
Daniel <shechiway@gmail.com>
2017-12-13 08:45:39 +0800

    [Q22][1.2%][v1]

--- 22/Solution.java
+++ 22/Solution.java
@@ -1,6 +1,20 @@
 import java.util.*;
+//[,,,,,,,,,,,,]
+//["","","","","","","(())(())","","","","","",""]
 class Solution {
     public List<String> generateParenthesis(int n) {
+        String[] test1 = {};
+        String[] test2 = {};
+
+        /*for ( String t : test2 ) {
+            int count = 0;
+            for ( String y : test1 ) {
+                if ( !y.equals(t) )
+                    count++;
+            }
+            if ( count == 131 )
+                System.out.println("not in:"+t);
+        }*/
         List<String> result = new ArrayList<>();
         if ( n == 0 ) {
             return result;
@@ -13,6 +27,7 @@ class Solution {
         result.add("(())");
         result.add("()()");
         for ( int i = 2 ; i < n ; i++ ) {
+            temp.clear();
             for ( String pattern : result ) {
                 temp.add(pattern);
             }
@@ -43,6 +58,41 @@ class Solution {
                         if ( !result.contains(internal) )
                             result.add(internal);
                     }
+                    if ( index != (patternArray.length - 1) &&
+                        patternArray[index] =='(' && 
+                        patternArray[index+1] ==')' ) {
+                        String internal = "";
+                        for ( int tempIndex = 0 ; tempIndex < patternArray.length ; tempIndex++ ) {
+                            if ( tempIndex != index )
+                                internal += patternArray[tempIndex];
+                            else {
+                                internal += patternArray[tempIndex];
+                                internal += "()";
+                            }
+                        }
+                        if ( !result.contains(internal) )
+                            result.add(internal);
+                    }
+                    if ( index <= (patternArray.length - 3) &&
+                        patternArray[index] =='(' && 
+                        patternArray[index+1] ==')' &&
+                        patternArray[index+2] =='(' && 
+                        patternArray[index+3] ==')' ) {
+                        String internal = "";
+                        for ( int tempIndex = 0 ; tempIndex < patternArray.length ; tempIndex++ ) {
+                            if (tempIndex == index) {
+                                internal += patternArray[tempIndex];
+                                internal += "(";
+                            } else if (tempIndex == index+3) {
+                                internal += patternArray[tempIndex];
+                                internal += ")";
+                            } else {
+                                internal += patternArray[tempIndex];
+                            }
+                        }
+                        if ( !result.contains(internal) )
+                            result.add(internal);
+                    }
                     if ( patternArray[index] == ')' && index == patternArray.length - 1 ) {;
                         String internal = pattern+"()";
                         if ( !result.contains(internal) )
@@ -57,8 +107,8 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.generateParenthesis(3));
-        System.out.println(solution.generateParenthesis(4));
-        System.out.println(solution.generateParenthesis(5));
+        //System.out.println(solution.generateParenthesis(3));
+        //System.out.println(solution.generateParenthesis(4));
+        System.out.println(solution.generateParenthesis(6));
     }
 }
