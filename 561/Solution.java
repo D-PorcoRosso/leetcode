24a661827dd0b30b8115e0acb3421ab5a963f636
Daniel <shechiway@gmail.com>
2018-01-10 23:42:12 +0800

    [Q561][84.61%][v1] change bubble sort to quick sort

--- 561/Solution.java
+++ 561/Solution.java
@@ -1,24 +1,33 @@
 class Solution {
 
-    private int[] bubbleSort(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            boolean isSwap = false;
-            for (int j = 0; j < nums.length - 1; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    int temp = nums[j + 1];
-                    nums[j + 1] = nums[j];
-                    nums[j] = temp;
-                    isSwap = true;
-                }
+    private int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
-            if (!isSwap)
-                break;
         }
-        return nums;
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public void quick_sort(int arr[], int left, int right) {
+        if (left < right) {
+            int pi = partition(arr, left, right);
+            quick_sort(arr, left, pi - 1);
+            quick_sort(arr, pi + 1, right);
+        }
     }
 
     public int arrayPairSum(int[] nums) {
-        nums = bubbleSort(nums);
+        quick_sort(nums, 0, nums.length -1);
         int sum = 0;
         for ( int i = 0 ; i < nums.length ; i++ ) {
             if (i%2 == 0)
