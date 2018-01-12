0f731217802ce6945503efdf89467b3b89b9cf7f
Daniel <shechiway@gmail.com>
2018-01-12 08:20:07 +0800

    [Q557][5.9%][v1]

---
+++ 557/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+class Solution {
+
+    public String reverseWords(String s) {
+        String result = "";
+        Stack<Character> stack = new Stack<>();
+        for ( int i = 0 ; i < s.length() ; i++ ) {
+            char current = s.charAt(i);
+            if ( current == ' ' ) {
+                String temp = "";
+                while ( !stack.isEmpty() ) {
+                    temp+= stack.pop();
+                }
+                temp += ' ';
+                result += temp;
+            } else {
+                stack.push(current);
+            }
+            if ( i == s.length()-1 ) {
+                String temp = "";
+                while (!stack.isEmpty()) {
+                    temp += stack.pop();
+                }
+                result += temp;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.reverseWords("Let's take LeetCode contest"));
+    }
+}
