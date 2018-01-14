4838fe1bc2898b9b1cb352711966a825490087bb
Daniel <shechiway@gmail.com>
2018-01-14 14:48:53 +0800

    [Q344][8.3%][v1] java String must using StringBuilder append in concate

---
+++ 344/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String reverseString(String s) {
+        StringBuilder result = new StringBuilder();
+        for ( int i = s.length() - 1 ; i >= 0 ; i-- ) {
+            result.append(s.charAt(i));
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.reverseString("hello"));
+    }
+}
