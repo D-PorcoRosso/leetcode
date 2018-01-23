3eb19f35a15cd6a672c3e15f8202277587f5b5fd
Daniel <shechiway@gmail.com>
2018-01-24 07:54:24 +0800

    [Q520][26.14%][v1]

---
+++ 520/Solution.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int wordLength = word.length();
+        
+        if ( wordLength == 0 ) 
+            return false;
+        if ( wordLength == 1 )
+            return true;
+
+        if ( Character.isUpperCase(word.charAt(0)) ) {
+            if ( Character.isUpperCase(word.charAt(1)) ) {
+                for ( int i = 2 ; i < word.length() ; i++ ) {
+                    if ( !Character.isUpperCase(word.charAt(i)) )
+                        return false;
+                }
+            } else {
+                for ( int i = 2 ; i < word.length() ; i++ ) {
+                    if ( Character.isUpperCase(word.charAt(i)) )
+                        return false;
+                }
+            }
+        } else {
+            for ( int i = 1 ; i < word.length() ; i++ ) {
+                if ( Character.isUpperCase(word.charAt(i)) )
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.detectCapitalUse("USA"));
+        System.out.println(solution.detectCapitalUse("leetcode"));
+        System.out.println(solution.detectCapitalUse("Google"));
+        System.out.println(solution.detectCapitalUse("GooGle"));
+        System.out.println(solution.detectCapitalUse("GoOGLE"));
+        System.out.println(solution.detectCapitalUse("G"));
+        System.out.println(solution.detectCapitalUse("Go"));
+        System.out.println(solution.detectCapitalUse("GO"));
+        System.out.println(solution.detectCapitalUse("oG"));
+        System.out.println(solution.detectCapitalUse("o"));
+        System.out.println(solution.detectCapitalUse("oo"));
+    }
+}
