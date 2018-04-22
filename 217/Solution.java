f1fec5d918cb56b5d7e666d37ea1f87a8c1adf04
shechiway <shechiway@gmail.com>
2018-04-22 11:55:54 +0800

    [Q217][time exceed][v2] using quick sort still time exceed

--- 217/Solution.java
+++ 217/Solution.java
@@ -2,15 +2,38 @@ import java.util.Arrays;
 
 class Solution {
 
+    private int partition(int arr[], int low, int high) throws Exception {
+        int pivot = arr[high];
+        int i = low-1;
+        for ( int j=low ; j<high ; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            } else if (arr[j] == pivot)
+                throw new Exception("equals");
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+
+        return i+1;
+    }
+
+    public void quick_sort(int arr[], int left, int right) throws Exception{
+        if ( left < right ) {
+            int pi = partition(arr, left, right);
+            quick_sort(arr, left, pi-1);
+            quick_sort(arr, pi+1, right);
+        }
+    }
+
     public boolean containsDuplicate(int[] nums) {
-        for ( int i = 0 ; i < nums.length ; i++ ) {
-            for ( int j = 0 ; j < nums.length ; j++ ) {
-                if ( i == j )
-                    break;
-                else
-                    if ( nums[i] == nums[j]) 
-                        return true;
-            }
+        try {
+            quick_sort(nums, 0, nums.length -1);
+        } catch (Exception e) {
+            return true;
         }
         return false;
     }
