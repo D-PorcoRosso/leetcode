078afc4dafb1c52d685226c24d42b3e2a2648ba4
shechiway <shechiway@gmail.com>
2018-04-22 12:14:04 +0800

    [Q217][77.11%][v3] using set to add it. but i think we need a bit wise version

--- 217/Solution.java
+++ 217/Solution.java
@@ -1,46 +1,19 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
 
-    private int partition(int arr[], int low, int high) throws Exception {
-        int pivot = arr[high];
-        int i = low-1;
-        for ( int j=low ; j<high ; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            } else if (arr[j] == pivot)
-                throw new Exception("equals");
-        }
-        int temp = arr[i+1];
-        arr[i+1] = arr[high];
-        arr[high] = temp;
-
-        return i+1;
-    }
-
-    public void quick_sort(int arr[], int left, int right) throws Exception{
-        if ( left < right ) {
-            int pi = partition(arr, left, right);
-            quick_sort(arr, left, pi-1);
-            quick_sort(arr, pi+1, right);
-        }
-    }
-
     public boolean containsDuplicate(int[] nums) {
-        try {
-            quick_sort(nums, 0, nums.length -1);
-        } catch (Exception e) {
-            return true;
+        HashSet<Integer> testDuplicate = new HashSet<Integer>();
+        for ( int num : nums ) {
+            if (!testDuplicate.add(num))
+                return true;
         }
         return false;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] test = {1,2,2,4,5};
+        int[] test = {1,2,3,4,5};
         System.out.println(solution.containsDuplicate(test));
 
     }
