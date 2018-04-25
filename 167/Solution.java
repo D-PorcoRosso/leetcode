2e41c3a1c57a01ff609ad12aab405947857336e2
shechiway <shechiway@gmail.com>
2018-04-25 15:21:28 +0800

    [Q167][26.94][v2]

--- 167/Solution.java
+++ 167/Solution.java
@@ -5,12 +5,19 @@ class Solution {
         int[] result = new int[2];
         result[0] = 0;
         result[1] = 0;
+        if ( target == 0 ) {
+            result[0] = 1;
+            result[1] = 2;
+        }
+
         for ( int i = 0 ; i < numbers.length ; i++ ) {
-            for ( int j = i+1 ; j < numbers.length ; j++ ) {
-                if ( (numbers[i]+numbers[j]) == target ) {
-                    result[0] = i+1;
-                    result[1] = j+1;
-                    return result;
+            if ( (i != numbers.length - 1) && !(numbers[i] == 0 && numbers[i+1] == 0) ) {
+                for ( int j = i+1 ; j < numbers.length ; j++ ) {
+                    if ( (numbers[i]+numbers[j]) == target ) {
+                        result[0] = i+1;
+                        result[1] = j+1;
+                        return result;
+                    }
                 }
             }
         }
@@ -26,6 +33,18 @@ class Solution {
         }
         int[] test1 = {0,0,3,4};
         results = solution.twoSum(test1, 0);
+        for ( int result : results ) {
+            System.out.println(result);
+        }
+
+        int[] test2 = {0,1,3,4};
+        results = solution.twoSum(test2, 1);
+        for ( int result : results ) {
+            System.out.println(result);
+        }
+
+        int[] test3 = {1,2,3,4,4,9,56,90};
+        results = solution.twoSum(test3, 8);
         for ( int result : results ) {
             System.out.println(result);
         }
