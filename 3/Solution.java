da3f8e105e14819ab04bde7241327d6c6e07351c
shechiway <shechiway@gmail.com>
2018-05-07 22:52:52 +0800

    [Q3][time exceed][v1] for loop 2 layer, although break on exist char shows, but using the list contains, so the time complexity is O(n^3)

---
+++ 3/Solution.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        List<Character> exists = new ArrayList<>();
+        int maxCount = 0, currentCount = 0;
+        for ( int i = 0 ; i < s.length() ; i++ ) {
+            for ( int j = i ; j < s.length() ; j++ ) {
+                Character c = s.charAt(j);
+                //System.out.println(c);
+                if ( exists.contains(c) ) {
+                    exists.clear();
+                    currentCount = 0;
+                    break;
+                } else {
+                    exists.add(c);
+                    currentCount++;
+                    if ( maxCount < currentCount )
+                        maxCount = currentCount;
+                }
+            }
+        }
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(solution.lengthOfLongestSubstring("bbbbb"));
+        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
+        System.out.println(solution.lengthOfLongestSubstring("c"));
+        System.out.println(solution.lengthOfLongestSubstring("aab"));
+        System.out.println(solution.lengthOfLongestSubstring("dvdf"));
+        System.out.println(solution.lengthOfLongestSubstring("asjrgapa"));
+        System.out.println(solution.lengthOfLongestSubstring("jbpnbwwd"));
+    }
+}
