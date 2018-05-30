442e398c8c140f7ea8606a8c5637853189ed5e5a
shechiway <shechiway@gmail.com>
2018-05-31 00:09:10 +0800

    [Q38][95.38%][v1] loop n minus two times , and each time loop all the char in the temp string, and count the same char, append the string when current char and pre character different, and count from start again, and at the end, if count not equal to zero, append the string again, after n minus two round, we will get the answer

--- 38/Solution.java
+++ 38/Solution.java
@@ -1,37 +1,40 @@
 
 class Solution {
     public String countAndSay(int n) {
-        int current = 1;
-        int outCount = 0;
-        String currentResult = "1",nextRound = "1";
-        for ( int i = 1 ; i <= n ; i++ ) {
-            //System.out.println(currentResult);
-            char currentChar = currentResult.charAt(0);
-            currentResult = nextRound;
-            nextRound = "";
-            for ( int j = 0 ; j < currentResult.length() ; ) {
-                if ( currentChar == currentResult.charAt(j)) {
-                    outCount++;
-                    j++;
+        if ( n == 0 )
+            return "";
+        if ( n == 1 )
+            return "1";
+        String returnString = "11";
+        for ( int i = 1 ; i < n - 1 ; i++ ) {
+            char[] array = returnString.toCharArray();
+            StringBuilder builder = new StringBuilder();
+            int count = 0;
+            char pre = array[0];
+            for ( int j = 0 ; j < array.length ; j++ ) {
+                if ( array[j] == pre ) {
+                    count++;
                 } else {
-                    nextRound += String.valueOf(outCount);
-                    nextRound += String.valueOf(currentChar);
-                    outCount = 0;
-                    currentChar = currentResult.charAt(j);
+                    builder.append(count);
+                    builder.append(pre);
+                    count = 1;
+                    pre = array[j];
                 }
             }
-            if ( nextRound.equals("") ) {
-                nextRound += String.valueOf(outCount);
-                nextRound += String.valueOf(currentChar);
+            if ( count != 0 ) {
+                builder.append(count);
+                builder.append(pre);
             }
-            currentResult = nextRound;
+            returnString = builder.toString();
+            System.out.println(returnString);
         }
-        return nextRound;
+        return returnString;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
     
-        System.out.println(solution.countAndSay(4));
+        System.out.println(solution.countAndSay(10));
+        //6 -> 2111121211
     }
 }
