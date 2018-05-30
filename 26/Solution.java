a68334a29f97cd13a28f2b4e3c157613d77abbc6
shechiway <shechiway@gmail.com>
2018-05-30 22:44:39 +0800

    [Q26][13.23%][v1] prepare a hashset for exist value validate, and for loop one layer, if not contains in hashset, then add it and count plus plus and save the current num to count index position

--- 26/Solution.java
+++ 26/Solution.java
@@ -1,20 +1,24 @@
+import java.util.HashSet;
 
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int length = nums.length;
-        if ( length == 0 )
-            return 0;
-        if ( length == 1 )
-            return 1;
-        int count = 1;
-        for ( int i = 0 ; i < length - 1; i++ ) {
-            if ( nums[i] != nums[i+1] ) {
+        int count = 0;
+        HashSet<Integer> storage = new HashSet<>();
+        for ( int i = 0 ; i < nums.length ; i++ ) {
+            if ( !storage.contains(nums[i]) ) {
+                storage.add(nums[i]);
+                nums[count] = nums[i];
                 count++;
             }
         }
-        if ( nums[length-1] != nums[length-2] )
-            count++;
         return count;
+    }
+
+    private static void print(int[] input) {
+        for ( int i : input ) {
+            System.out.print(i+",");
+        }
+        System.out.print("\n");
     }
 
     public static void main(String[] args) {
@@ -24,10 +28,18 @@ class Solution {
         int[] t3 = {1};
         int[] t4 = {1,2,3,4,5,6,7,8,9};
         int[] t5 = {1,2,3,4,5,6,7,8,8,8,8,8};
+        int[] t6 = {0,0,1,1,1,2,2,3,3,4};
         System.out.println(solution.removeDuplicates(t1));
+        print(t1);
         System.out.println(solution.removeDuplicates(t2));
+        print(t2);
         System.out.println(solution.removeDuplicates(t3));
+        print(t3);
         System.out.println(solution.removeDuplicates(t4));
+        print(t4);
         System.out.println(solution.removeDuplicates(t5));
+        print(t5);
+        System.out.println(solution.removeDuplicates(t6));
+        print(t6);
     }
 }
