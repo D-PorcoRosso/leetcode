558aaaacabfb12f35ff9bd3f5740d2092c611709
shechiway <shechiway@gmail.com>
2018-05-30 23:08:54 +0800

    [Q27][99.06%][v1] for loop one layer, O(n) , and compare current number to val, if not equal assign the i-th nums to the index of nums , index plus plus

--- 27/Solution.java
+++ 27/Solution.java
@@ -1,18 +1,25 @@
 
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int length = nums.length;
+        int index = 0;
         int count = 0;
-        if ( length == 0 )
+
+        if ( nums.length == 0 )
             return 0;
-        for ( int i = 0 ; i < length ; i++ ) {
+        for ( int i = 0 ; i < nums.length ; i++ ) {
             if ( nums[i] != val ) {
-                count++;
-            } else {
-                nums[i] = -1;
+                nums[index] = nums[i];
+                index++;
             }
         }
-        return count;
+        return index;
+    }
+
+    private static void print(int[] input) {
+        for ( int i : input ) {
+            System.out.print(i+",");
+        }
+        System.out.print("\n");
     }
 
     public static void main(String[] args) {
@@ -20,12 +27,17 @@ class Solution {
         int[] t1 = {1,1,1};
         int[] t2 = {};
         int[] t3 = {1};
-        int[] t4 = {1,2,3,4,5,6,7,8,9};
+        int[] t4 = {0,1,2,2,3,0,4,2};
         int[] t5 = {1,2,3,4,5,6,7,8,8,8,8,8};
         System.out.println(solution.removeElement(t1, 3));
+        print(t1);
         System.out.println(solution.removeElement(t2, 3));
+        print(t2);
         System.out.println(solution.removeElement(t3, 3));
-        System.out.println(solution.removeElement(t4, 3));
+        print(t3);
+        System.out.println(solution.removeElement(t4, 2));
+        print(t4);
         System.out.println(solution.removeElement(t5, 3));
+        print(t5);
     }
 }
