bf9cd17e88db9f39593113ecbf6f39f13973bcbc
shechiway <shechiway@gmail.com>
2018-05-31 14:20:54 +0800

    [Q101][81.21%][V1] reference the discussion, recursive compare (left.right , right.left) && (left.left, right.right)

--- 101/Solution.java
+++ 101/Solution.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
     static class TreeNode {
@@ -14,48 +12,21 @@ class Solution {
     }
 
     public boolean isSymmetric(TreeNode root) {
-
         if ( root == null )
             return true;
+        
+        return isSymmetric(root.left, root.right);
+    }
 
-        Queue<TreeNode> bfsQueue = new LinkedList<>();
-        ArrayList<Integer> flatNode = new ArrayList<>();
-
-        bfsQueue.offer(root);
-
-        while(!bfsQueue.isEmpty()) {
-            TreeNode node = bfsQueue.poll();
-            if ( node != null ) {
-                flatNode.add(node.val);
-                bfsQueue.offer(node.left);
-                bfsQueue.offer(node.right);
-            } else {
-                flatNode.add(null);
-            }
-        }
-
-        int power2 = 1, sum = 1, index = 0, power2SubIndex = 1;
-        for ( ; index < flatNode.size() ; index++ ) {
-            try {
-                if ( flatNode.get(index) == flatNode.get(power2SubIndex-1) ) {
-                    power2SubIndex--;
-                } else
-                    return false;
-            } catch ( IndexOutOfBoundsException e ) {
-                return false;
-            }
-            if ( index != flatNode.size() - 1) {
-                if ( index == sum - 1 ) {
-                    power2 *= 2;
-                    sum += power2;
-                    power2SubIndex = sum;
-                }
-            }
-        }
-        if ( index != sum )
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if ( left == null && right == null ) {
+            return true;
+        } else if ( left == null || right == null ) {
             return false;
-
-        return true;
+        } else if ( left.val == right.val ) {
+            return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+        }
+        return false;
     }
 
     public static void main(String[] args) {
