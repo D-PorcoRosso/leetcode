949b0262507fe33d4a42e2bba9c2a92fdfba7029
shechiway <shechiway@gmail.com>
2018-05-31 10:42:23 +0800

    [Q69][6.99%][V1] for loop from index 1 to x, and square each index and break when the negative shows on x minus index's square and return the result index

---
+++ 69/Solution.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public int mySqrt(int x) {
+        int result = 0;
+        for ( int i = 1 ; i <= x ; i++ ) {
+            int square = i*i;
+            if ( (x - square) < 0 ){
+                break;
+            }
+            result = i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.mySqrt(2));
+        System.out.println(solution.mySqrt(4));
+        System.out.println(solution.mySqrt(8));
+        System.out.println(solution.mySqrt(9));
+        System.out.println(solution.mySqrt(100));
+        System.out.println(solution.mySqrt(1000));
+    }
+}
