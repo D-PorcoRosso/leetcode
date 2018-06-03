4093d578c471f265fe370d9ce2cba41d966aaa5f
shechiway <shechiway@gmail.com>
2018-06-03 11:42:44 +0800

    [Q49][66.27%][v2] reference

--- 49/Solution.java
+++ 49/Solution.java
@@ -3,48 +3,18 @@ import java.util.*;
 class Solution {
 
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, HashMap<Character, Integer>> allCount = new HashMap<>();
-        for ( String s : strs ) {
-            HashMap<Character, Integer> count = new HashMap<>();
-            char[] cs = s.toCharArray();
-            for ( char c : cs ) {                
-                if (count.get(c) == null)
-                    count.put(c, 1);
-                else {
-                    int tempCount = count.get(c);
-                    count.put(c,++tempCount);
-                }
-            }
-            allCount.put(s, count);
-        }
-        int length = strs.length;
-        boolean[] alreadyDone = new boolean[length];
         List<List<String>> result = new ArrayList<>();
-        for ( int i = 0 ; i < length ; i++ ) {
-            List<String> anagrams = new ArrayList<>();
-            if (!alreadyDone[i]) {
-                String current = strs[i];
-                anagrams.add(current);
-                HashMap<Character, Integer> count = allCount.get(current);
-                for ( int j = i + 1 ; j < length ; j++) {
-                    HashMap<Character, Integer> compareCount = allCount.get(strs[j]);
-                    if ( count.size() != compareCount.size() )
-                        continue;
-                    int localCount = 0;
-                    for ( Map.Entry<Character, Integer> countEntry : count.entrySet() ) {
-                        if ( countEntry.getValue() == compareCount.get(countEntry.getKey()) )
-                            localCount++;
-                        else
-                            break;
-                    }
-                    if ( localCount == count.size() ) {
-                        anagrams.add(strs[j]);
-                        alreadyDone[j] = true;
-                    }
-                }
-                result.add(anagrams);
-            }
+        HashMap<String , ArrayList<String>> accumulate = new HashMap<>();
+        for ( String s : strs ) {
+            char[] cs = s.toCharArray();
+            Arrays.sort(cs);
+            String key = String.valueOf(cs);
+            if (!accumulate.containsKey(key))
+                accumulate.put(key, new ArrayList<String>());
+            accumulate.get(key).add(s);
         }
+        for ( List<String> s : accumulate.values() )
+            result.add(s);
         return result;
     }
 
@@ -54,7 +24,7 @@ class Solution {
         //System.out.println(solution.groupAnagrams(test1));
         String[] test2 = {"eat", "tea", "tan", "ate", "nat", "stressed", "desserts"};
         //System.out.println(solution.groupAnagrams(test2));
-        String[] test3 = {"boo","bob",""};
+        String[] test3 = {"fat","asp",""};
         System.out.println(solution.groupAnagrams(test3));
     }
 }
