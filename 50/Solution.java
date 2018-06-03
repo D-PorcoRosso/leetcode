92d68e391151695a6985a634024287120a0e4cc6
shechiway <shechiway@gmail.com>
2018-06-03 13:27:31 +0800

    [Q50][85.40%][v1] reference

---
+++ 50/Solution.java
@@ -0,0 +1,28 @@
+
+class Solution {
+
+    public double myPow(double x, int n) {
+        if (n == 0)
+            return 1;
+        if ( n < 0 ) {
+            if ( n == Integer.MIN_VALUE ) {
+                n = Integer.MAX_VALUE;
+                if ( x < 0 )
+                    x = -x;
+            } else {
+                n = -n;
+            }
+            x = 1/x;
+        }
+        return n % 2 == 0? myPow(x*x, n/2):x*myPow(x*x, n/2);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        /*System.out.println(solution.myPow(2,10));
+        System.out.println(solution.myPow(2.1,3));
+        System.out.println(solution.myPow(2,-10));*/
+        System.out.println(solution.myPow(0.00001,2147483647));
+    }
+}
