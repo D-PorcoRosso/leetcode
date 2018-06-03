6a632db1d38f746905d49f2bb28e1fff4d3bbbfc
shechiway <shechiway@gmail.com>
2018-06-03 20:59:04 +0800

    [Q55][24.96%][v3] reference

--- 55/Solution.java
+++ 55/Solution.java
@@ -2,50 +2,13 @@ import java.util.*;
 
 class Solution {
 
-    private class Status {
-
-        Status(boolean isVisit, boolean status) {
-            this.isVisit = isVisit;
-            this.status = status;
-        }
-        public final boolean isVisit;
-        public final boolean status;
-    }
-
     public boolean canJump(int[] nums) {
-        Status[] status = new Status[nums.length];
-        return isExist(0,nums,status);
-    }
-
-    private boolean isExist(int index, int[] nums, Status[] status) {
-        int length = nums.length;
-        
-        if (status[index] != null && status[index].isVisit){
-            //System.out.println(index+":"+status[index].status);
-            return status[index].status;
+        int localMax = 0;
+        for ( int i = 0 ; i < nums.length ; i++ ) {
+            if ( i > localMax ) return false;
+            localMax = Math.max(localMax, i+nums[i]);
         }
-        //System.out.println(index+":"+nums[index]);
-        if ( index == length - 1 ) {
-            Status indexStatus = new Status(true,true);
-            status[index] = indexStatus;
-            return true;
-        }
-        if ( nums[index] == 0 ) {
-            Status indexStatus = new Status(true,false);
-            status[index] = indexStatus;
-            return false;
-        }
-        boolean isExist = false;
-        for ( int i = 1 ; i <= nums[index] ; i++ ) {
-            if ( index+i < length ) {
-                isExist |= isExist(index+i, nums,status);
-                Status indexStatus = new Status(true,isExist);
-                status[index] = indexStatus;
-            }
-            if (isExist)
-                return isExist;
-        }
-        return isExist;
+        return true;
     }
 
     public static void main(String[] args) {
