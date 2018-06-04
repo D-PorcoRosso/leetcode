805fe4acf5c8c16797d37ab869902fb0e9a5ba41
shechiway <shechiway@gmail.com>
2018-06-04 15:10:21 +0800

    [Q62][87.71%][v3]

--- 62/Solution.java
+++ 62/Solution.java
@@ -3,24 +3,15 @@ import java.util.*;
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] matrix = new int[m][n];
-        for (int i = 0 ; i < n ; i++) {
-            matrix[0][i] = 1;
+        for ( int i = 0 ; i < m ; i++ ) {
+            for ( int j = 0 ; j < n ; j++ ) {
+                if ( i == 0 || j == 0 ) matrix[i][j] = 1;
+                else {
+                    matrix[i][j] = matrix[i-1][j] + matrix[i][j-1];
+                }
+            }
         }
-        for (int i = 1 ; i < m ; i++) {
-            matrix[i][0] = 1;
-        }
-        return uniquePathsWithSaver(matrix, m, n);
-    }
-
-    private int uniquePathsWithSaver(int[][] matrix, int m, int n) {
-        
-        if ( matrix[m-1][n-1] != 0 ) {
-            System.out.println(matrix[m-1][n-1]);
-            return matrix[m-1][n-1];
-        }
-        int result = uniquePathsWithSaver(matrix, m-1, n) + uniquePathsWithSaver(matrix, m, n-1);
-        matrix[m-1][n-1] = result;
-        return result;
+        return matrix[m-1][n-1];
     }
 
     public static void main(String[] args) {
