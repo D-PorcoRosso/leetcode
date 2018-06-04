4d8474e2003b0280da870612dd1ea208fa239b12
shechiway <shechiway@gmail.com>
2018-06-04 17:43:44 +0800

    [Q73][Sol]

--- 73/Solution.java
+++ 73/Solution.java
@@ -1,6 +1,40 @@
 import java.util.*;
 
 class Solution {
+
+    /*
+        public void setZeroes(int[][] matrix) {
+        boolean firstRow=false,firstCol=false;
+        int m=matrix[0].length,n=matrix.length;
+        
+        for(int i=0;i<m;i++)
+            if(matrix[0][i]==0)firstRow=true;
+        for(int i=0;i<n;i++)
+            if(matrix[i][0]==0)firstCol=true;
+        //use the firstRow and the firstCol as buffer
+        for(int i=1;i<n;i++)
+            for(int j=1;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        
+        for(int i=1;i<m;i++)
+            if(matrix[0][i]==0)
+                for(int j=1;j<n;j++)
+                    matrix[j][i]=0;
+        for(int i=1;i<n;i++)
+            if(matrix[i][0]==0)
+                for(int j=1;j<m;j++)
+                    matrix[i][j]=0;
+        
+        if(firstRow)for(int i=0;i<m;i++)matrix[0][i]=0;
+        if(firstCol)for(int i=0;i<n;i++)matrix[i][0]=0;
+    } */
+
     static class Point {
         public final int x;
         public final int y;
