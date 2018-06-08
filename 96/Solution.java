a818bcfaa978ca709dddb2826520298561e627a5
shechiway <shechiway@gmail.com>
2018-06-08 10:02:26 +0800

    [Q96][100%][v2] use array

--- 96/Solution.java
+++ 96/Solution.java
@@ -3,18 +3,15 @@ class Solution {
     public int numTrees(int n) {
         int[] count = new int[n+1];
         count[0] = 1; count[1] = 1;
-        return counting(count, n);
-    }
-
-    private int counting(int[] count, int n) {
-        if ( n == 1 || n == 0 )
-            return 1;
         int result = 0;
-        for ( int i = 0 ; i < n ; i++ ) {
-            result += (counting(count, i)*counting(count, n-1-i));
+        for ( int num = 2 ; num <= n ; num++ ) {
+            for ( int i = 0 ; i < num ; i++ ) {
+                result += (count[i]*count[num-1-i]);
+            }
+            count[num] = result;
+            result = 0;
         }
-        count[n] = result;
-        return result;
+        return count[n];
     }
 
     public static void main(String[] args) {
