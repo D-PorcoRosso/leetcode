6fdaa9ca77aa5faa723816f2862c517530a48b43
shechiway <shechiway@gmail.com>
2018-06-13 22:41:22 +0800

    random generate test input

--- src/mysource/SortingTest.java
+++ src/mysource/SortingTest.java
@@ -6,9 +6,9 @@ import mysource.utils.RandomTest;
 public class SortingTest {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] test1 = RandomTest.genRandomIntArray(10);
+        int[] test1 = RandomTest.genRandomIntArray(100);
         solution.quick_sort(test1, 0, test1.length - 1);
         for ( int t : test1 )
-            System.out.print(t+",");
+            System.out.println(t+",");
     }
 }
--- src/mysource/utils/RandomTest.java
+++ src/mysource/utils/RandomTest.java
@@ -1,9 +1,15 @@
 package mysource.utils;
 
+import java.util.Random;
+
 public class RandomTest {
 
     public static int[] genRandomIntArray(int size) {
-        int[] result ={ 2, 5, 7, 8, 9, 3, 12, 11, 4, 1, 87 };//= new int[size];
+        int[] result = new int[size];
+        Random rand = new Random();
+        for ( int i = 0 ; i < size ; i++ ) {
+            result[i] = rand.nextInt();
+        }
         return result;
     }
 }
