afd229f9644fb80ff62777e72783cdf8a36bfedd
shechiway <shechiway@gmail.com>
2021-08-20 23:15:39 +0800

    [75random] 11

--- src/mysource/Solution11/Solution.java
+++ src/mysource/Solution11/Solution.java
@@ -1,10 +1,10 @@
 package mysource.Solution11;
 
 class Solution {
+    // o(n)
     public int maxArea(int[] height) {
         int left = 0, right = height.length-1;
         int max = Integer.MIN_VALUE;
-        int rightHeight = height[right], leftHight = height[left];
         while ( left < right ) {
             int localMax = Math.min(height[left],height[right]) * (right - left);
             if (localMax > max)
@@ -17,13 +17,36 @@ class Solution {
         return max;
     }
 
+    //O(n^2) 
+    public int maxArea2021(int[] height) {
+        int max = 0;
+        for ( int index = 0 ; index < height.length ; index++) {
+            int start = height.length, end = 0;
+            for (int pivot = 0 ; pivot < height.length ; pivot++ ) {
+                if (height[pivot] - height[index] >= 0 && pivot < start) {
+                    start = pivot;
+                }
+                if (height[pivot]-height[index] >= 0 && pivot > end) {
+                    end = pivot;
+                }
+            }
+            int localMax = height[index] * (end -start);
+            if (max < localMax) {
+                max = localMax;
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] test1 = {1,2,3,4,5};
-        System.out.println(solution.maxArea(test1));
+        System.out.println(solution.maxArea2021(test1));
         int[] test2 = {1,1};
-        System.out.println(solution.maxArea(test2));
+        System.out.println(solution.maxArea2021(test2));
         int[] test3 = {1,2,1};
-        System.out.println(solution.maxArea(test3));
+        System.out.println(solution.maxArea2021(test3));
+        int[] test4 = {1,8,6,2,5,4,8,3,7};
+        System.out.println(solution.maxArea2021(test4));
     }
 }
