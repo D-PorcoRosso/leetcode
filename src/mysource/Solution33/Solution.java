930bb4c07844451d48b86b064cf80734f5f6812f
shechiway <shechiway@gmail.com>
2021-08-30 23:29:33 +0800

    [75random] 33

--- src/mysource/Solution33/Solution.java
+++ src/mysource/Solution33/Solution.java
@@ -43,6 +43,23 @@ class Solution {
         }
         return -1;
     }
+    
+    public int search_2021_logn(int[] nums, int target) {
+        int start = 0, end = nums.length - 1, mid;
+        while (start <= end) {
+            mid = start + (end-start)/2;
+            if (target == nums[mid])
+                return mid;
+            if (nums[mid] >= nums[start]) {
+                if (target >= nums[start] && target < nums[mid] ) end = mid -1;
+                else start = mid +1;
+            } else {
+                if (target <= nums[end] && target > nums[mid]) start = mid + 1;
+                else end = mid -1;
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
