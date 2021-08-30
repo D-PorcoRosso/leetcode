dbdaedde3fb4b297fb463eb2afcdba71f91e27c1
shechiway <shechiway@gmail.com>
2021-08-30 22:42:12 +0800

    [75random] 33

--- src/mysource/Solution33/Solution.java
+++ src/mysource/Solution33/Solution.java
@@ -36,6 +36,14 @@ class Solution {
         return -1;
     }
 
+    public int search_2021(int[] nums, int target) {
+        for ( int i = 0 ; i < nums.length ; i++ ){
+            if (target == nums[i])
+                return i;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] test1 = {0,1,2,3,4,5,6,7,8,9};
