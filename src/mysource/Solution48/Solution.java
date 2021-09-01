26d42583b6abde2ec65414c62018261dad978e9c
shechiway <shechiway@gmail.com>
2021-09-01 17:30:48 +0800

    [75random] 48

--- src/mysource/Solution48/Solution.java
+++ src/mysource/Solution48/Solution.java
@@ -20,6 +20,25 @@ class Solution {
         }
     }
 
+    public void rotate_2021(int[][] matrix) {
+        int n = matrix.length;
+        for ( int i = 0 ; i < n ; i++ ) {
+            for ( int j = i ; j < n ; j++ ) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        for ( int i = 0 ; i < n ; i++ ) {
+            for ( int j = 0 ; j < n/2 ; j++ ) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[][] test1 = {
