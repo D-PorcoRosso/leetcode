661fa3f23fb451deef537af847bb6dd8cba9ca7d
shechiway <shechiway@gmail.com>
2021-09-01 22:15:26 +0800

    [75random] 49

--- src/mysource/Solution49/Solution.java
+++ src/mysource/Solution49/Solution.java
@@ -23,6 +23,35 @@ class Solution {
         return result;
     }
 
+    public List<List<String>> groupAnagrams_2021(String[] strs) {
+        List<HashMap<Character, Integer>> compare = new ArrayList<>();
+        List<List<String>> results = new ArrayList<>();
+        HashMap<HashMap<Character, Integer>, List<String>> hashResults = new HashMap<>();
+        for ( int i = 0 ; i < strs.length ; i++ ) {
+            String str = strs[i];
+            HashMap<Character, Integer> anagram = new HashMap<>();
+            for ( int j = 0 ; j < str.length() ; j++ ) {
+                char currentCh = str.charAt(j);
+                if (anagram.get(currentCh) == null) {
+                    anagram.put(currentCh, 1);
+                } else {
+                    anagram.put(currentCh, anagram.get(currentCh)+1);
+                }
+            }
+            if (compare.contains(anagram)){
+                hashResults.get(anagram).add(str);
+            } else {
+                compare.add(anagram);
+                List<String> tempStr = new ArrayList<>();
+                tempStr.add(str);
+                results.add(tempStr);
+                hashResults.put(anagram, tempStr);
+            }
+        }
+        return results;        
+        
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         String[] test1 = {"eat", "tea", "tan", "ate", "nat", "bat"};
