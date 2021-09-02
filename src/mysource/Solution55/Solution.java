275a6b80783834e2136ccc5790a5c39b3cc2b900
shechiway <shechiway@gmail.com>
2021-09-02 15:55:57 +0800

    [75random] 55

--- src/mysource/Solution55/Solution.java
+++ src/mysource/Solution55/Solution.java
@@ -11,6 +11,22 @@ class Solution {
         return true;
     }
 
+    public boolean canJump_2021(int[] nums) {
+        int currentMax = nums[0];
+        if (currentMax == 0 && nums.length > 1)
+            return false;
+        for ( int i = 1 ; i < nums.length -1 ; i++) {
+            if (currentMax-1 < nums[i]) {
+                currentMax = nums[i];
+            } else {
+                currentMax--;
+            }
+            if (currentMax == 0)
+                return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] test1 = {2,3,1,1,4};
