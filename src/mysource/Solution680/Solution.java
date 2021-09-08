9f25be8b7bcc7c13f38bfa4c7950c15d9261cab7
shechiway <shechiway@gmail.com>
2021-09-08 14:54:13 +0800

    solution 680 pass

--- src/mysource/Solution680/Solution.java
+++ src/mysource/Solution680/Solution.java
@@ -22,6 +22,42 @@ public class Solution {
         }
         return returnArray;
     }
+
+    public boolean validPalindrome_pass(String s) {
+        char[] str = s.toCharArray();
+        if (isPalindrome(str))
+            return true;
+        int length = str.length;
+        int indexBack = length - 1;
+        boolean isExistOneDelete = false;
+        for ( int index = 0 ; index <= indexBack ; ) {
+            if (str[index] != str[indexBack]) {
+                if (!isExistOneDelete) {
+                    if (index == indexBack-1)
+                        return true;
+                    boolean isSubStringPalind = false;
+                    if (str[index] == str[indexBack -1]) {
+                        char[] subString = new char[indexBack-1-index+1];
+                        for ( int i = 0 ; i < subString.length ; i++ ) {
+                            subString[i] = str[index+i];
+                        }
+                        isSubStringPalind = isSubStringPalind || isPalindrome(subString);
+                    }
+                    if (str[index+1] == str[indexBack]) {
+                        char[] subString = new char[indexBack-index-1+1];
+                        for ( int i = 0 ; i < subString.length ; i++ ) {
+                            subString[i] = str[index+1+i];
+                        }
+                        isSubStringPalind = isSubStringPalind || isPalindrome(subString);
+                    }
+                    return isSubStringPalind;
+                }
+            }
+            index++;
+            indexBack--;
+        }
+        return true;
+    }
     
     private boolean isPalindrome(char[] array) {
         int length = array.length;
