5b5b140e20c0efbeef8da1091b3622d0fcfd5e85
shechiway <shechiway@gmail.com>
2021-09-10 12:20:45 +0800

    [75random] 191

--- src/mysource/Solution191/Solution.java
+++ src/mysource/Solution191/Solution.java
@@ -1,13 +1,23 @@
 package mysource.Solution191;
 
 class Solution {
+    // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int count = 0;
-        while( n > 0 ) {
-            if ( (n%2) == 1 ) {
+        double target;
+        if ( n < 0 ) {
+            target = Integer.MAX_VALUE + 1 + n;
+            count = 1;
+        } else 
+            target = n;
+        int index = 32;
+        while( index > 0 ) {
+            double current = Math.pow(2,index-1);
+            if ( (target/current) >= 1.0 ) {
+                target -= current;
                 count++;
             }
-            n = n >> 1;
+            index--;
         }
         return count;
     }
