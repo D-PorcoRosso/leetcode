55e92d792bd054cf061d7e2c1934f0982b7c9313
shechiway <shechiway@gmail.com>
2021-09-11 17:23:21 +0800

    [75random] 208 replace by trie data structure

--- src/mysource/Solution208/Solution.java
+++ src/mysource/Solution208/Solution.java
@@ -1,30 +1,78 @@
 package mysource.Solution208;
 
+class TrieNode {
+    private TrieNode[] links;
+    private final int R = 26;
+    private boolean isEnd;
+    
+    public TrieNode() {
+        links = new TrieNode[R];
+    }
+    
+    public boolean containsKey(char ch) {
+        return links[ch-'a'] != null;
+    }
+    
+    public TrieNode get(char ch) {
+        return links[ch-'a'];
+    }
+    
+    public void put(char ch, TrieNode node) {
+        links[ch-'a'] = node;
+    }
+    
+    public void setEnd() {
+        isEnd = true;
+    }
+    
+    public boolean isEnd() {
+        return isEnd;
+    }
+}
+
 class Trie {
 
-    List<String> trie;
+    private TrieNode root;
+    
     /** Initialize your data structure here. */
     public Trie() {
-        trie = new ArrayList<>();
+        root = new TrieNode();
     }
     
     /** Inserts a word into the trie. */
     public void insert(String word) {
-        if (!trie.contains(word))
-            trie.add(word);
+        TrieNode node = root;
+        char[] wordArray = word.toCharArray();
+        for ( int i = 0 ; i < wordArray.length ; i++ ) {
+            if (!node.containsKey(wordArray[i])) {
+                node.put(wordArray[i], new TrieNode());
+            }
+            node = node.get(wordArray[i]);
+        }
+        node.setEnd();
     }
     
     /** Returns if the word is in the trie. */
     public boolean search(String word) {
-        return trie.contains(word);
+        TrieNode node = root;
+        char[] wordArray = word.toCharArray();
+        for ( int i = 0 ; i < wordArray.length ; i++ ) {
+            node = node.get(wordArray[i]);
+            if (node == null)
+                return false;
+        }
+        return node != null && node.isEnd();
     }
     
     /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
-        for (String s : trie) {
-            if (s.startsWith(prefix))
-                return true;
+        TrieNode node = root;
+        char[] wordArray = prefix.toCharArray();
+        for ( int i = 0 ; i < wordArray.length ; i++ ) {
+            node = node.get(wordArray[i]);
+            if (node == null)
+                return false;
         }
-        return false;
+        return true;
     }
 }
