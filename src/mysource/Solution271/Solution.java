ae15969afd153697070503d8862f21bed6bc44e0
shechiway <shechiway@gmail.com>
2021-09-12 22:53:24 +0800

    [75random] 271 pass

--- src/mysource/Solution271/Solution.java
+++ src/mysource/Solution271/Solution.java
@@ -1,6 +1,29 @@
 package mysource.Solution271;
 
 public class Solution {
+
+    // Encodes a list of strings to a single string.
+    public String encode_usingoutchar(List<String> strs) {
+        if (strs.size() == 0) return Character.toString((char)257);
+        String d = Character.toString((char)258);
+        StringBuilder sb = new StringBuilder();
+        for ( String s : strs ) {
+            sb.append(s);
+            sb.append(d);
+        }
+        sb.deleteCharAt(sb.length()-1);
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode_usingoutchar(String s) {
+        String d = Character.toString((char)257);
+        if (s.equals(d)) return new ArrayList();
+        
+        d = Character.toString((char)258);
+        return Arrays.asList(s.split(d,-1));
+    }
+
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
         String encoded = "";
