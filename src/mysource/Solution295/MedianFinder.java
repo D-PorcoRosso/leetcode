7b99325cc345c2a84a1cde6b993bbb8a8272b707
shechiway <shechiway@gmail.com>
2021-09-13 11:58:30 +0800

    [75random] 295 with priorityqueue

--- src/mysource/Solution295/MedianFinder.java
+++ src/mysource/Solution295/MedianFinder.java
@@ -32,3 +32,33 @@ class MedianFinder {
  * obj.addNum(num);
  * double param_2 = obj.findMedian();
  */
+
+class MedianFinder_2 {
+
+    PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
+    PriorityQueue<Integer> large = new PriorityQueue<>();
+    boolean even = true;
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+    }
+    
+    public void addNum(int num) {
+        if (even) {
+            large.offer(num);
+            small.offer(large.poll());
+        } else {
+            small.offer(num);
+            large.offer(small.poll());
+        }
+        even = !even;
+    }
+    
+    public double findMedian() {
+        if (even) {
+            return (small.peek() + large.peek()) / 2.0;
+        } else {
+            return small.peek();
+        }
+    }
+}
