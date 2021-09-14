7a097755b55daaf7bdbf2385c7e4ac1e1dd84a9b
shechiway <shechiway@gmail.com>
2021-09-14 10:42:15 +0800

    [75random] 347 but slow

--- src/mysource/Solution347/Solution.java
+++ src/mysource/Solution347/Solution.java
@@ -39,6 +39,33 @@ public class Solution {
         return result;
     }
 
+    public int[] topKFrequent_2021(int[] nums, int k) {
+        HashMap<Integer, Integer> mapping = new HashMap<>();
+        for ( int i = 0 ; i < nums.length ; i++ ) {
+            if ( mapping.get(nums[i]) == null ) {
+                mapping.put(nums[i],1);
+            } else {
+                int count = mapping.get(nums[i]);
+                mapping.put(nums[i],++count);
+            }
+        }
+        
+        int[] returnElems = new int[k];
+        while( k > 0 ) {
+            int max = -1, index = 0;
+            for ( Map.Entry<Integer, Integer> entry : mapping.entrySet() ) {
+                if (entry.getValue() >= max) {
+                    max = entry.getValue();
+                    index = entry.getKey();
+                    returnElems[k-1] = index;
+                }
+            }
+            mapping.put(index, 0);
+            k--;
+        }
+        return returnElems;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] test = { 1,1,1,2,2,3 };
