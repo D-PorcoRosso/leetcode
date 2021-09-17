711455db6e0e79cb9f9e8c5b68a0cb70534f5636
shechiway <shechiway@gmail.com>
2021-09-17 18:07:13 +0800

    [75random] 91

---
+++ src/mysource/Solution91/Solution.java
@@ -0,0 +1,70 @@
+package mysource.Solution91;
+
+public class Solution {
+    public int numDecodings(String s) {
+        char[] array = s.toCharArray();
+        int[] dpTable = new int[array.length];
+        for ( int i = 0 ; i < dpTable.length ; i++ ) {
+            dpTable[i] = -1;
+        }
+        return isValid(array, dpTable, array.length -1);
+    }
+    
+    private int isValid (char[] array, int[] dpTable, int index) {
+        if (index < 0) {
+            return 1;
+        }
+        if (index == 0) {
+            dpTable[0] = array[0] == '0' ? 0 : 1;
+            return dpTable[0];
+        }
+        int mappingOne = array[index] == '0' ? -1 : Integer.parseInt(""+array[index]);
+        int mappingTwo = array[index-1] == '0' ? -1 : Integer.parseInt(new StringBuilder().append(array[index-1]).append(array[index]).toString());
+        if (index == 1) {
+            if (pass(mappingOne) && pass(mappingTwo)) {
+                dpTable[1] = 2;
+            }
+            else if (!pass(mappingOne) && pass(mappingTwo)) {
+                dpTable[1] = 1;
+            }
+            else if (pass(mappingOne) && !pass(mappingTwo)){
+                if (dpTable[index-1] == -1) {
+                    dpTable[index] = isValid(array, dpTable, index-1);
+                }
+            } else {
+                dpTable[1] = 0;
+            }
+            return dpTable[1];
+        }
+        
+        if (pass(mappingOne) && pass(mappingTwo)) {
+            if (dpTable[index-1] == -1) {
+                dpTable[index-1] = isValid(array, dpTable, index-1);
+            }
+            
+            if (dpTable[index-2] == -1) {
+                dpTable[index-2] = isValid(array, dpTable, index-2);
+            }
+            return dpTable[index-1] + dpTable[index-2];
+        }
+        if (pass(mappingOne) && !pass(mappingTwo)) {
+            if (dpTable[index-1] == -1) {
+                dpTable[index-1] = isValid(array, dpTable, index-1);
+            }
+            return dpTable[index-1];
+        }
+        if (!pass(mappingOne) && pass(mappingTwo)) {
+            if (dpTable[index-2] == -1) {
+                dpTable[index-2] = isValid(array, dpTable, index-2);
+            }
+            return dpTable[index-2];
+        }
+        return 0;
+    }
+    
+    private boolean pass(int mappingIndex) {
+        if (mappingIndex < 0 || mappingIndex > 26)
+            return false;
+        return true;
+    }
+}
