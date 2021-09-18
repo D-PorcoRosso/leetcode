444819e9b584225380df72f8b224dfd90019634e
shechiway <shechiway@gmail.com>
2021-09-18 16:48:22 +0800

    [75random] 152, o(n^2)

--- src/mysource/Solution152/Solution.java
+++ src/mysource/Solution152/Solution.java
@@ -1,6 +1,19 @@
 package mysource.Solution152;
 
 public class Solution {
+    public int maxProduct_2021(int[] nums) {
+        int product = 1, maxProduct = Integer.MIN_VALUE;
+        for ( int i = 0 ; i < nums.length ; i++ ) {
+            product = 1;
+            for ( int j = i ; j < nums.length ; j++ ) {
+                product *= nums[j];
+                if (product > maxProduct)
+                    maxProduct = product;
+            }
+        }
+        return maxProduct;
+    }
+
     public int maxProduct(int[] nums) {
         int max = Integer.MIN_VALUE;
         for ( int productCount = 1 ; productCount <= nums.length ; productCount++ ) {
