dcd4a7f980662ccd98094f314bd419135ad5f2f8
shechiway <shechiway@gmail.com>
2021-09-18 22:48:48 +0800

    [250] 3

--- src/mysource/Solution3/Solution.java
+++ src/mysource/Solution3/Solution.java
@@ -3,6 +3,31 @@ package mysource.Solution3;
 import java.util.HashMap;
 
 class Solution {
+    public int lengthOfLongestSubstring_2021_250(String s) {
+        char[] str = s.toCharArray();
+        int i = 0 , j = 0, max = Integer.MIN_VALUE;
+        List<Character> container = new ArrayList<>();
+        while (j<s.length()) {
+            if (!container.contains(str[j])) {
+                container.add(str[j]);
+                j++;
+            } else {
+                char target = str[j];
+                while(str[i] != target) {
+                    Character c = str[i];
+                    container.remove(c);
+                    i++;
+                }
+                i++;
+                j++;
+            }
+            max = Math.max(max, j-i);
+        }
+        if (max == Integer.MIN_VALUE)
+            max = 0;
+        return max;
+    }
+
     public int lengthOfLongestSubstring(String s) {
         HashMap<Character,Integer> exists = new HashMap<>();
         int maxCount = 0, currentCount = 0;
