225f39bf6caf58eb63fc973c5c3726c12020e8f9
shechiway <shechiway@gmail.com>
2021-09-19 18:33:53 +0800

    [250] 13

--- src/mysource/Solution13/Solution.java
+++ src/mysource/Solution13/Solution.java
@@ -34,6 +34,60 @@ class Solution {
         return sum;
     }
 
+    //CM : 900 CD: 400, XC:90, XL:40, IX: 9, IV: 4
+    public int romanToInt_2021(String s) {
+        
+        if ( s == null || s.length() == 0 )
+            return 0;
+        char[] str = s.toCharArray();
+        int result = 0;
+        for ( int i = 0 ; i < str.length ; i++ ) {
+            if (str[i] == 'M') {
+                result += 1000;
+            }
+            else if (str[i] == 'C') {
+                if (i < str.length-1 && str[i+1] == 'M') {
+                    i++;
+                    result += 900;
+                } else if (i < str.length-1 && str[i+1] == 'D') {
+                    i++;
+                    result += 400;
+                } else 
+                    result += 100;
+            }
+            else if (str[i] == 'D') {
+                result += 500;
+            }
+            else if (str[i] == 'X') {
+                if (i < str.length-1 && str[i+1] == 'C') {
+                    i++;
+                    result += 90;
+                } else if (i < str.length-1 && str[i+1] == 'L') {
+                    i++;
+                    result += 40;
+                } else 
+                    result += 10;
+            }
+            else if (str[i] == 'I') {
+                if (i < str.length-1 && str[i+1] == 'X') {
+                    i++;
+                    result += 9;
+                } else if (i < str.length-1 && str[i+1] == 'V') {
+                    i++;
+                    result += 4;
+                } else 
+                    result += 1;
+            }
+            else if (str[i] == 'L') {
+                result += 50;
+            }
+            else if (str[i] == 'V') {
+                result += 5;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
        
