d00a42932dd391cfb59c6745b05cfa17c1b4107b
shechiway <shechiway@gmail.com>
2021-09-19 10:54:04 +0800

    [250] 4, merge two array and get medium

--- src/mysource/Solution4/Solution.java
+++ src/mysource/Solution4/Solution.java
@@ -2,7 +2,36 @@ package mysource.Solution4;
 
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        return 0;
+        int total = nums1.length + nums2.length;
+        int target = total/2;
+
+        int[] mergedArray = new int[nums1.length+nums2.length];
+        int i = 0, j = 0; 
+        int boundary = 0;
+        while ( boundary < target +1 && boundary < nums1.length+nums2.length) {
+            if (i == nums1.length && nums2.length != 0) {
+                mergedArray[boundary]=nums2[j];
+                j++;
+            } else if (j == nums2.length && nums1.length != 0) {
+                mergedArray[boundary]=nums1[i];
+                i++;
+            }
+            if (i < nums1.length && j < nums2.length ) {
+                if (nums1[i] > nums2[j]) {
+                    mergedArray[boundary]=nums2[j];
+                    j++;
+                } else {
+                    mergedArray[boundary]=nums1[i];
+                    i++;
+                }
+            }
+            boundary++;
+        }
+        if ( total % 2 == 0 ) {
+            return (mergedArray[target] + mergedArray[target-1])/2.0;
+        } else {
+            return mergedArray[target];
+        }
     }
 
     public static void main(String[] args) {
