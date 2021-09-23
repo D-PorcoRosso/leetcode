df3a5173cd850370e3cadb8e5d5a4dd72f17084a
shechiway <shechiway@gmail.com>
2021-09-24 01:49:49 +0800

    [250] 33

--- src/mysource/Solution33/Solution.java
+++ src/mysource/Solution33/Solution.java
@@ -60,6 +60,27 @@ class Solution {
         }
         return -1;
     }
+    
+    public int search_2021_250(int[] nums, int target) {
+        int start = 0, end = nums.length - 1, mid = 0;
+        while( start <= end ) {
+            mid = (end - start)/2+ start;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] >= nums[start]) {
+                if ( nums[start] <= target && target < nums[mid])
+                    end = mid - 1;
+                else 
+                    start = mid + 1;
+            } else {
+                if ( target <= nums[end] && target > nums[mid])
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
