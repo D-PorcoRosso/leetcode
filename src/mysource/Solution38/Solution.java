2fd8efa72decdd3616124a56b65a34f9abd919f5
shechiway <shechiway@gmail.com>
2021-09-25 00:14:01 +0800

    [250] 38

--- src/mysource/Solution38/Solution.java
+++ src/mysource/Solution38/Solution.java
@@ -1,6 +1,56 @@
 package mysource.Solution38;
 
 class Solution {
+    public String countAndSay_2021_250(int n) {
+        String result = "1";
+        if (n == 1) {
+            return result;
+        }
+        
+        result = "11";
+        if (n == 2) {
+            return result;
+        }
+        
+        int index = 2;
+        while ( index < n ) {
+            result = parseString(result);
+            index++;
+        }
+        return result;
+    }
+    
+    private String parseString (String str) {
+        char[] arr = str.toCharArray();
+        String result = "";
+        int count = 1;
+        int currentNum = Character.getNumericValue(arr[0]);
+        StringBuilder builder = new StringBuilder();
+        for ( int i = 1 ; i < arr.length ; i++ ) {
+            if (currentNum == Character.getNumericValue(arr[i])) {
+                count++;
+            } else {
+                builder.append(result);
+                builder.append(count);
+                builder.append(currentNum);
+                currentNum = Character.getNumericValue(arr[i]);
+                count = 1;
+                if (i == arr.length -1) {
+                    builder.append(result);
+                    builder.append(count);
+                    builder.append(currentNum);
+                }
+            }
+        }
+        if (count != 1) {
+            builder.append(result);
+            builder.append(count);
+            builder.append(currentNum);
+        }
+        
+        return builder.toString();
+    }
+
     public String countAndSay(int n) {
         if ( n == 0 )
             return "";
