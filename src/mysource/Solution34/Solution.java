bb343008626a94167fecb6006e00955e066aee57
shechiway <shechiway@gmail.com>
2021-09-24 22:34:15 +0800

    [250] 34

--- src/mysource/Solution34/Solution.java
+++ src/mysource/Solution34/Solution.java
@@ -1,6 +1,60 @@
 package mysource.Solution34;
 
 class Solution {
+
+    public int[] searchRange_2021_250_allbinarysearch(int[] nums, int target) {
+        int[] result = new int[2];
+        if (nums.length == 0) {
+            result[0] = -1;
+            result[1] = -1;
+            return result;
+        }
+        int firstResult = binarySearch(nums, target, 0, nums.length-1);
+        if ( firstResult == -1 ) {
+            result[0] = -1;
+            result[1] = -1;
+            return result;
+        } else {
+            int start = 0, end = firstResult - 1, startIndex = -2, endIndex = -2, rangeStart = -1, rangeEnd = -1;
+            while (startIndex != -1 && start >=0 && end>=0) {
+                startIndex = binarySearch(nums, target, start, end);
+                if (startIndex != -1)
+                    rangeStart = startIndex;
+                end = startIndex - 1;
+            }
+            start = firstResult + 1;
+            end = nums.length-1;
+            while (endIndex != -1 && start < nums.length && end < nums.length) {
+                endIndex = binarySearch(nums, target, start, end);
+                if (endIndex != -1)
+                    rangeEnd = endIndex;
+                start = endIndex + 1;
+            }
+            if (rangeStart < 0)
+                rangeStart = firstResult;
+            if (rangeEnd < 0)
+                rangeEnd = firstResult;
+            result[0] = rangeStart;
+            result[1] = rangeEnd;
+            return result;
+        }
+    }
+    
+    private int binarySearch(int[] nums, int target, int start, int end) {
+        int mid = -1;
+        while (start <= end) {
+            mid = (end-start)/2 + start;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     public int[] searchRange(int[] nums, int target) {
         int start = 0, end = nums.length - 1;
         int[] result = {-1,-1};
