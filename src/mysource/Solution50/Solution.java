e6e473e63b0dd1cb6268a1fdce48805e090f90dd
shechiway <shechiway@gmail.com>
2021-09-26 16:40:02 +0800

    [250] 50

--- src/mysource/Solution50/Solution.java
+++ src/mysource/Solution50/Solution.java
@@ -2,6 +2,45 @@ package mysource.Solution50;
 
 class Solution {
 
+    public double myPow_2021_250(double x, int n) {
+
+        boolean isFraction = false;
+        if ( n < 0 ) {
+            n = 0 - n;
+            isFraction = true;
+        }
+        
+        boolean isNagtive = false;
+        if (x < 0) {
+            x = 0-x;
+            if (n%2 != 0) {
+                isNagtive = true;
+            }
+        }
+        if (x == 1.0) {
+            if (isNagtive)
+                return -1.0;
+            else
+                return 1.0;
+        }
+        
+        double powResult = 1.0;
+        if ( n == Integer.MIN_VALUE)
+            n = Integer.MAX_VALUE;
+        for ( int i = n ; i > 0 ; i-- ) {
+            if (powResult < 0.00001)
+                break;
+            if (powResult >= 100000)
+                break;
+            powResult = powResult *x;
+        }
+        
+        if (isFraction) {
+            return isNagtive ? 0- 1.0/powResult : 1.0/powResult;
+        }
+        return isNagtive ? 0-powResult : powResult;
+    }
+
     public double myPow(double x, int n) {
         if (n == 0)
             return 1;
