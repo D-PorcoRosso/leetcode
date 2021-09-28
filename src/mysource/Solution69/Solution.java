9492f8175dfbd6a92a608a5468be132f1401f9c1
shechiway <shechiway@gmail.com>
2021-09-28 17:49:57 +0800

    [250] 69

--- src/mysource/Solution69/Solution.java
+++ src/mysource/Solution69/Solution.java
@@ -13,6 +13,21 @@ class Solution {
         return result;
     }
 
+    public int mySqrt_2021_250(int x) {
+        for ( int i = 1 ; i <= x ; i++ ) {
+            if ( i*i == x) {
+                return i;
+            }
+            if ( i*i > x) {
+                return i-1;
+            }
+            if ( i*i < 0) {
+                return i-1;
+            }
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.mySqrt(2));
