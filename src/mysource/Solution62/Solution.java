35471ef93fd9a711f182e369bc8d90f844a61191
shechiway <shechiway@gmail.com>
2021-09-28 15:52:42 +0800

    [250] 62

--- src/mysource/Solution62/Solution.java
+++ src/mysource/Solution62/Solution.java
@@ -14,6 +14,27 @@ class Solution {
         return matrix[m-1][n-1];
     }
 
+    public int uniquePaths_2021_250(int m, int n) {
+        int[][] dpTable = new int[m][n];
+        for ( int i = 0 ; i < m ; i++) {
+            dpTable[i][0] = 1;
+        }
+        for ( int i = 0 ; i < n ; i++) {
+            dpTable[0][i] = 1;
+        }
+        return findPath(dpTable, m-1,n-1);
+    }
+    private int findPath(int[][] dpTable, int i, int j) {
+        if (i == 0)
+            return dpTable[0][j];
+        if (j == 0)
+            return dpTable[i][0];
+        if (dpTable[i][j] != 0)
+            return dpTable[i][j];
+        dpTable[i][j] = findPath(dpTable, i-1, j) + findPath(dpTable, i, j-1);
+        return dpTable[i][j];
+    }
+
     public int uniquePaths_2021(int m, int n) {
         int[][] dp = new int[m][n];
         for( int i = 0 ; i < m ; i++ ) {
