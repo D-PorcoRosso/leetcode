0326ad8e5450db7ae751fdde2aea8ff3664248d7
shechiway <shechiway@gmail.com>
2021-10-02 17:41:19 +0800

    [250] 96

--- src/mysource/Solution96/Solution.java
+++ src/mysource/Solution96/Solution.java
@@ -1,6 +1,19 @@
 package mysource.Solution96;
 
 class Solution {
+
+    public int numTrees_2021_250(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
     public int numTrees(int n) {
         int[] count = new int[n+1];
         count[0] = 1; count[1] = 1;
