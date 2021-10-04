e194e7cf9f38286ad924eb66c7b57200195d6496
shechiway <shechiway@gmail.com>
2021-10-04 12:29:24 +0800

    [250] 124

--- src/mysource/Solution124/Solution.java
+++ src/mysource/Solution124/Solution.java
@@ -16,4 +16,22 @@ public class Solution {
         max = Math.max(max, left+right+root.val);
         return Math.max(left,right)+root.val;
     }
+
+    private int max = Integer.MIN_VALUE;
+    public int maxPathSum_2021_250(TreeNode root) {
+        findMax(root);
+        return max;
+    }
+    
+    private int findMax(TreeNode root) {
+        if (root == null)
+            return 0;
+        
+        int rightMax = findMax(root.right);
+        int leftMax = findMax(root.left);
+        int localMax = Math.max(root.val, Math.max( root.val+leftMax, Math.max(root.val+rightMax, (root.val + rightMax + leftMax))));
+        if (localMax > max)
+            max = localMax;
+        return Math.max(root.val, root.val + Math.max(rightMax, leftMax));
+    }
 }
