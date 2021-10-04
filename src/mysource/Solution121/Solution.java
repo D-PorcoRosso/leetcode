5c46fca35ed96bb608cb0ea04f9330083862546e
shechiway <shechiway@gmail.com>
2021-10-04 11:25:56 +0800

    [250] 121 timeout

--- src/mysource/Solution121/Solution.java
+++ src/mysource/Solution121/Solution.java
@@ -1,6 +1,17 @@
 package mysource.Solution121;
 
 class Solution {
+
+    public int maxProfit_2021_250_1(int[] prices) {
+        int max = 0;
+        for ( int i = 0 ; i < prices.length ; i++ ) {
+            for ( int j = i + 1 ; j < prices.length ; j++ )
+                if (prices[j] - prices[i] > max) 
+                    max = prices[j] - prices[i];
+        }
+        return max;
+    }
+
     public int maxProfit(int[] prices) {
         int diff = 0;
         if (prices == null || prices.length ==0)
