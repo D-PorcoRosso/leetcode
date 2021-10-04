21d54fd51728bd4e338375b1458a0a5b06022d7e
shechiway <shechiway@gmail.com>
2021-10-04 11:39:08 +0800

    [250] 122

--- src/mysource/Solution122/Solution.java
+++ src/mysource/Solution122/Solution.java
@@ -1,6 +1,24 @@
 package mysource.Solution122;
 
 class Solution {
+    public int maxProfit_2021_250(int[] prices) {
+        int max = 0;
+        for ( int i = 0 ; i < prices.length ; ) {
+            int j = i+1;
+            if ( j < prices.length && prices[i] > prices[j] ) 
+                i++;
+            else{
+                int localMax = 0;
+                while (j < prices.length && localMax <= prices[j] - prices[i]) {
+                    localMax = prices[j] - prices[i];
+                    j++;
+                }
+                i = j;
+                max += localMax;
+            }
+        }
+        return max;
+    }
     public int maxProfit(int[] prices) {
         int length = prices.length;
         int max = 0;
