8fe9a15a029a8468599d616aad14541f143f52bb
shechiway <shechiway@gmail.com>
2021-10-05 18:08:45 +0800

    [250] 139

--- src/mysource/Solution139/Solution.java
+++ src/mysource/Solution139/Solution.java
@@ -1,19 +1,27 @@
 package mysource.Solution139;
 
 public class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
-        return isExist(s, wordDict);
+    public boolean wordBreak(String s, List<String> wordDict) {    
+        Boolean[] dpTable = new Boolean[s.length()];
+        return isExist(s, wordDict, 0, dpTable);
     }
     
-    private boolean isExist (String s, List<String> wordDict) {
+    private boolean isExist (String s, List<String> wordDict, int index, Boolean[] dpTable) {
         if (s.length() == 0)
             return true;
+        if (dpTable[index] != null)
+            return dpTable[index];
         boolean isExist = false;
         for ( String word : wordDict) {
             if (isMatch(s, word)) {
-                isExist |= isExist(s.substring(word.length(), s.length()), wordDict);
+                isExist |= isExist(s.substring(word.length(), s.length()), wordDict, index+word.length(), dpTable);
+                if (isExist) {
+                    dpTable[index] = true;
+                    return true;
+                }
             }
         }
+        dpTable[index] = isExist;
         return isExist;
     }
     
