e289cd73900fb289f654d7a45808f6f04b7d57c5
shechiway <shechiway@gmail.com>
2021-10-05 12:18:23 +0800

    [250] 133

--- src/mysource/Solution133/Solution.java
+++ src/mysource/Solution133/Solution.java
@@ -1,6 +1,28 @@
 package mysource.Solution133;
 
 public class Solution {
+
+    HashMap<Integer, Node> saveNode = new HashMap<>();
+    public Node cloneGraph_2021)250(Node node) {
+        if (node == null)
+            return null;
+        return findAllNode(node);
+    }
+    
+    private Node findAllNode(Node node) {
+        if (saveNode.get(node.val) != null)
+            return saveNode.get(node.val);
+        
+        Node newNode = new Node(node.val);
+        saveNode.put(node.val, newNode);
+        List<Node> temp = new ArrayList<>();
+        for ( Node n : node.neighbors) {
+            temp.add(findAllNode(n));
+        }
+        newNode.neighbors = temp;
+        return newNode;
+    }
+
     public HashMap<Integer, Node> saveMap;
     public Node cloneGraph(Node node) {
         saveMap = new HashMap<>();
