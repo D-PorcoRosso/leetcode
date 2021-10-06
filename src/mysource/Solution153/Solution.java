f0ac16e41a8010445a14932a8614f42160e1fbfd
shechiway <shechiway@gmail.com>
2021-10-06 18:03:49 +0800

    [250] 153

--- src/mysource/Solution153/Solution.java
+++ src/mysource/Solution153/Solution.java
@@ -1,6 +1,26 @@
 package mysource.Solution153;
 
 public class Solution {
+    public int findMin_2021_250(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
+        int start = 0, end = nums.length-1, mid = 0;
+        while (start <= end) {
+            mid = (end-start)/2 + start;
+            if (start == end) break;
+            if (nums[mid] > nums[mid+1])
+                return nums[mid+1];
+            if (mid-1 >= 0 && nums[mid] < nums[mid-1])
+                return nums[mid];
+            if (nums[mid] > nums[end]) {
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return nums[start];
+    }
+
     public int findMin(int[] nums) {
         if (nums.length == 1)
             return nums[0];
