ec71ab7ed0460e267c1e8c879f5a2dc2c62b3820
shechiway <shechiway@gmail.com>
2021-10-06 11:08:22 +0800

    [250] 142

--- src/mysource/Solution142/Solution.java
+++ src/mysource/Solution142/Solution.java
@@ -1,7 +1,18 @@
 package mysource.Solution142;
 
 public class Solution {
-    public ListNode detectCycle(ListNode head) {
+    public ListNode detectCycle_2021_250_fast(ListNode head) {
+        Set<ListNode> saveNode = new HashSet<>();
+        while(head != null) {
+            if (saveNode.contains(head))
+                return head;
+            saveNode.add(head);
+            head = head.next;
+        }
+
+        return null;
+    }
+    public ListNode detectCycle_2021_250_slow(ListNode head) {
         ListNode twoStep = head;
         ListNode oneStep = head;
         while(head != null) {
