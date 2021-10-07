6053c27310eb34268143520831f72894aec11b68
shechiway <shechiway@gmail.com>
2021-10-07 23:15:43 +0800

    bubble sort

--- src/mysource/BubbleSort/Solution.java
+++ src/mysource/BubbleSort/Solution.java
@@ -1,6 +1,24 @@
 package mysource.BubbleSort;
 
 class Solution {
+
+    public void bubbleSort_2021(int[] nums) {
+        for ( int i = 0 ; i < nums.length ; i++ ) {
+            boolean isSwap = false;
+            for ( int j = 0 ; j < nums.length - 1 ; j++) {
+                if (nums[j] > nums[j+1]) {
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                    isSwap = true;
+                }
+            }
+            if (!isSwap)
+                break;
+        }
+    }
+
+
     public int[] bubbleSort(int[] nums) {
         for ( int i = 0 ; i < nums.length ; i++ ) {
             boolean isSwap = false;
@@ -22,9 +40,9 @@ class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] test1 = {2,5,7,8,9,3,12,11,4,1,87};
-        int[] result1 = solution.bubbleSort(test1);
-        for ( int i = 0 ; i < result1.length ; i++ ) {
-            System.out.println(result1[i]);
+        solution.bubbleSort_2021(test1);
+        for ( int i = 0 ; i < test1.length ; i++ ) {
+            System.out.println(test1[i]);
         }
     }
 }
