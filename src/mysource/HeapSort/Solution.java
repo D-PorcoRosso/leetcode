245736eb2239a22522c9a4a88b7b593c1b932e9d
shechiway <shechiway@gmail.com>
2021-10-08 11:46:49 +0800

    heapSort

--- src/mysource/HeapSort/Solution.java
+++ src/mysource/HeapSort/Solution.java
@@ -2,6 +2,41 @@ package mysource.HeapSort;
 
 class Solution {
 
+    private void swap(int[]arr, int i , int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public void heapSort_2021(int[] arr, int size) {
+        int left, right, startPoint = arr.length - 1 - size;
+        if (startPoint == 0)
+            return;
+        for (int j = startPoint; j >= 0 ; j--) {
+            left = 2*j+1;
+            right = 2*j+2;
+            if (left > startPoint)
+                continue;
+            if (right > startPoint) {
+                if (arr[j] < arr[left]) {
+                    swap(arr, j, left);
+                }
+            } else {
+                if (arr[left] < arr[right]) {
+                    if (arr[right] > arr[j]) {
+                        swap(arr, right,j);
+                    }
+                } else {
+                    if (arr[left] > arr[j]) {
+                        swap(arr, left,j);
+                    }
+                }
+            }
+        }
+        swap(arr, 0, startPoint);
+        heapSort_2021(arr, size+1);
+    }
+
     public void heapSort(int[] arr, int size) {
         
         int leftNodeIndex , rightNodeIndex, temp, length = arr.length-size;
@@ -36,25 +71,25 @@ class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] test1 = { 2, 5, 7, 8, 9, 3, 12, 11, 4, 1, 87 };
-        solution.heapSort(test1,0);
+        solution.heapSort_2021(test1,0);
         for (int i = 0; i < test1.length; i++) {
             System.out.print(test1[i]+",");
         }
         System.out.println();
         int[] test2 = { 1,2,3,4,5 };
-        solution.heapSort(test2,0);
+        solution.heapSort_2021(test2,0);
         for (int i = 0; i < test2.length; i++) {
             System.out.print(test2[i]+",");
         }
         System.out.println();
         int[] test3 = { 5,4,3,2,1 };
-        solution.heapSort(test3,0);
+        solution.heapSort_2021(test3,0);
         for (int i = 0; i < test3.length; i++) {
             System.out.print(test3[i]+",");
         }
         System.out.println();
         int[] test4 = { 1,1,1,1 };
-        solution.heapSort(test4,0);
+        solution.heapSort_2021(test4,0);
         for (int i = 0; i < test4.length; i++) {
             System.out.print(test4[i]+",");
         }
