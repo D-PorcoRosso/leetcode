f8c0b797d870fe4d3fd25963644c3ec036e18b31
shechiway <shechiway@gmail.com>
2021-10-08 10:53:13 +0800

    insertion sort

--- src/mysource/InsertionSort/Solution.java
+++ src/mysource/InsertionSort/Solution.java
@@ -1,6 +1,25 @@
 package mysource.InsertionSort;
 
 class Solution {
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void insertionSort_2021(int[] nums) {
+        for ( int i = 1 ; i < nums.length ; i++ ) {
+            int targetValue = nums[i];
+            for ( int j = i-1 ; j >=0 ; j-- ) {
+                if (targetValue < nums[j]) {
+                    swap(nums, j, j+1);
+                } else 
+                    break;
+            }
+        }
+    }
+
     public int[] insertionSort(int[] nums) {
         for (int i = 1 ; i < nums.length; i++) {
             int temp = nums[i];
@@ -15,10 +34,10 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] test1 = { 2, 5, 7, 8, 9, 3, 12, 11, 4, 1, 87 };
-        int[] result1 = solution.insertionSort(test1);
-        for (int i = 0; i < result1.length; i++) {
-            System.out.println(result1[i]);
+        int[] test1 = { 87 , 2, 5, 7, 8, 9, 3, 12, 11, 4, 1};
+        solution.insertionSort_2021(test1);
+        for (int i = 0; i < test1.length; i++) {
+            System.out.println(test1[i]);
         }
     }
 }
