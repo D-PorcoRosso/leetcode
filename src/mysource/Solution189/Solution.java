ba029146fb762bdcfa8e9b9d93c0612595be488a
shechiway <shechiway@gmail.com>
2021-10-11 11:48:01 +0800

    [250] 189

---
+++ src/mysource/Solution189/Solution.java
@@ -0,0 +1,38 @@
+package mysource.Solution189;
+
+public class Solution {
+    public void rotate_tle(int[] nums, int k) {
+        while (k > 0) {
+            k--;
+            int temp = nums[nums.length-1];
+            for (int i = nums.length-1 ; i > 0 ; i-- ) {
+                nums[i] = nums[i-1];
+            }
+            nums[0] = temp;
+        }
+    }
+
+    public void rotate_2021_250(int[] nums, int k) {
+        if (nums.length == 1)
+            return;
+        if (k > nums.length) {
+            k = k%nums.length;
+        }
+        for ( int i = 0 ; i < nums.length/2 ; i++ ) {
+            swap(nums, i, nums.length-1-i);
+        }
+        for (int i = 0 ; i < k/2 ; i++) {
+            swap(nums, i, k-1-i);
+        }
+        
+        for (int i = k ; i < (nums.length-k)/2 + k ; i++) {
+            swap(nums, i, nums.length-1+k-i);
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
