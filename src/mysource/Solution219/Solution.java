1d502b274ddbff05df88fa8b438aa0b0a47387aa
shechiway <shechiway@gmail.com>
2021-10-15 14:55:28 +0800

    [250] 219

--- src/mysource/Solution219/Solution.java
+++ src/mysource/Solution219/Solution.java
@@ -1,6 +1,22 @@
 package mysource.Solution219;
 
 public class Solution {
+    public boolean containsNearbyDuplicate_2021_250(int[] nums, int k) {
+        HashMap<Integer, Integer> mem = new HashMap<>();
+        for ( int i = 0 ; i < nums.length ; i++ ) {
+            if (mem.get(nums[i]) != null) {
+                if (i - mem.get(nums[i]) <= k)
+                    return true;
+                else{
+                    mem.put(nums[i], i);
+                }
+            } else {
+                mem.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer, List<Integer>> existTable = new HashMap<>();
         for (int i = 0 ; i < nums.length ; i++) {
