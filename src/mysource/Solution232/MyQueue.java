7e475320886ae77709a8d109f281db59ad3bc6e1
shechiway <shechiway@gmail.com>
2021-10-18 11:36:08 +0800

    [250] 232

--- src/mysource/Solution232/MyQueue.java
+++ src/mysource/Solution232/MyQueue.java
@@ -9,33 +9,38 @@ public class MyQueue {
     }
     
     public void push(int x) {
+        if (!stack2.isEmpty()) {
+            while(stack2.size() > 0) {
+                stack1.push(stack2.pop());
+            }
+        }
         stack1.push(x);
     }
     
     public int pop() {
+        if (stack1.isEmpty()) 
+            return stack2.pop();
         while(stack1.size() > 1) {
             stack2.push(stack1.pop());
         }
-        int returnValue = stack1.pop();
-        while(stack2.size() > 0) {
-            stack1.push(stack2.pop());
-        }
-        return returnValue;
+        return stack1.pop();
     }
     
     public int peek() {
         int returnValue = 0;
+        if (stack1.isEmpty()) {
+            returnValue = stack2.pop();
+            stack2.push(returnValue);
+            return returnValue;
+        }
         while(stack1.size() > 0) {
             returnValue = stack1.pop();
             stack2.push(returnValue);
-        }
-        while(stack2.size() > 0) {
-            stack1.push(stack2.pop());
         }
         return returnValue;
     }
     
     public boolean empty() {
-        return stack1.isEmpty();
+        return stack1.isEmpty() && stack2.isEmpty();
     }
 }
