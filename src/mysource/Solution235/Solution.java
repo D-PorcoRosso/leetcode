6eeea47896c5021e76f1beadfb13c63d7f7da867
shechiway <shechiway@gmail.com>
2021-10-18 15:59:07 +0800

    [250] 235

--- src/mysource/Solution235/Solution.java
+++ src/mysource/Solution235/Solution.java
@@ -1,6 +1,20 @@
 package mysource.Solution235;
 
 public class Solution {
+    public TreeNode lowestCommonAncestor_2021_250(TreeNode root, TreeNode p, TreeNode q) {
+        return findAncestor(root, p, q);
+    }
+    
+    private TreeNode findAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if ((q.val <= root.val && root.val <= p.val) || (p.val <= root.val && root.val <= q.val))
+            return root;
+        
+        if (root.val > p.val && root.val > q.val) {
+            return findAncestor(root.left, p, q);
+        }
+        return findAncestor(root.right, p, q);
+    }
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if ((root.val >= p.val && root.val <= q.val) || (root.val <= p.val && root.val >= q.val))
             return root;
