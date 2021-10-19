bcc9c76e8b4e78896ee0fd96e92c990248118472
shechiway <shechiway@gmail.com>
2021-10-19 18:10:38 +0800

    [250] 252

--- src/mysource/Solution252/Solution.java
+++ src/mysource/Solution252/Solution.java
@@ -1,6 +1,22 @@
 package mysource.Solution252;
 
 public class Solution {
+    public boolean canAttendMeetings_2021_250(int[][] intervals) {
+        if (intervals.length == 0)
+            return true;
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                return a[0]-b[0];
+            }
+        });
+        
+        for ( int i = 1 ; i < intervals.length ; i++ ) {
+            if (intervals[i][0] < intervals[i-1][1])
+                return false;
+        }
+        return true;
+    }
+
     public boolean canAttendMeetings(int[][] intervals) {
         if (intervals.length == 0)
             return true;
