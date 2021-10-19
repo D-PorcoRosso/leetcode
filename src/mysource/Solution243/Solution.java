1cf513af8d2dd05ef849bfa2411e6586608463ea
shechiway <shechiway@gmail.com>
2021-10-19 15:25:42 +0800

    [250] 243

--- src/mysource/Solution243/Solution.java
+++ src/mysource/Solution243/Solution.java
@@ -1,6 +1,23 @@
 package mysource.Solution243;
 
 public class Solution {
+
+    public int shortestDistance_2021_250_fast50percent(String[] wordsDict, String word1, String word2) {
+        int word1Index = -1, word2Index = -1, min = wordsDict.length;
+        for ( int i = 0 ; i < wordsDict.length ; i++ ) {
+            if(wordsDict[i].equals(word1)) {
+                word1Index = i;
+            }
+            else if (wordsDict[i].equals(word2))
+                word2Index = i;
+            if (word1Index != -1 && word2Index != -1) {
+                min = Math.min(min, Math.abs(word1Index-word2Index));
+            }
+        }
+        
+        return min;
+    }
+
     public int shortestDistance_2021_250_fasteralittle(String[] wordsDict, String word1, String word2) {
         int word1Index = -1, word2Index = -1, min = Integer.MAX_VALUE;
         for ( int i = 0 ; i < wordsDict.length ; i++ ) {
