e53c3c7a80d3ed211d5173b151dac25235572bf5
shechiway <shechiway@gmail.com>
2021-10-23 11:42:37 +0800

    [250] 257

---
+++ src/mysource/Solution257/Solution.java
@@ -0,0 +1,33 @@
+package mysource.Solution257;
+
+public class Solution {
+    public List<String> binaryTreePaths_2021_250(TreeNode root) {
+        if (root == null)
+            return new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        findBinaryTreePaths(root, result, "");
+        return result;
+    }
+    
+    private void findBinaryTreePaths(TreeNode root, List<String> result, String currentPath) {
+        if (currentPath.equals(""))
+            currentPath = ""+root.val;
+        else
+            currentPath = currentPath + "->" + root.val;
+        
+        if (root.left == null && root.right == null) {
+            result.add(currentPath);
+            return;
+        } else if (root.left == null && root.right != null) {
+            findBinaryTreePaths(root.right, result, currentPath);
+        } else if (root.left != null && root.right == null) {
+            findBinaryTreePaths(root.left, result, currentPath);
+        } else {
+            findBinaryTreePaths(root.right, result, currentPath);
+            findBinaryTreePaths(root.left, result, currentPath);
+        }
+        
+        
+        
+    }
+}
