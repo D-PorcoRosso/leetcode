92b54569252a21ab81771de7571c68cbf94a99b7
shechiway <shechiway@gmail.com>
2021-10-24 23:19:18 +0800

    [250] 268

--- src/mysource/Solution268/Solution.java
+++ src/mysource/Solution268/Solution.java
@@ -1,6 +1,22 @@
 package mysource.Solution268;
 
 public class Solution {
+    public int missingNumber_2021_250(int[] nums) {
+        
+        int sum = (nums.length)*(nums.length+1)/2;
+        int target = 0;
+        boolean isExistZero = false;
+        for (int n : nums) {
+            if (n == 0)
+                isExistZero = true;
+            target += n;
+        }
+        if (target == sum) {
+            return isExistZero ? nums.length : 0;
+        }
+        return sum-target;
+    }
+
     public int missingNumber(int[] nums) {
         int[] container = new int[nums.length+1];
         for ( int i = 0 ; i < nums.length ; i++ ) {
