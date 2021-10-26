14d734a8eee9a2ac383e086a913abac21cdfa9fe
shechiway <shechiway@gmail.com>
2021-10-26 16:22:45 +0800

    [amz] 1963

---
+++ src/mysource/Solution1963/Solution.java
@@ -0,0 +1,55 @@
+package mysource.Solution1963;
+
+public class Solution {
+    public int minSwaps_amz_TLE(String s) {
+        int start = 0, end = s.length()-1;
+        int count = 0;
+        char[] temp = s.toCharArray();
+        while (start < end) {
+            if (isBalanced(temp)) {
+                return count;
+            }
+            while(start < temp.length/2) {
+                if(temp[start] == ']') {
+                    break;
+                }
+                start++;
+            }
+            
+            while(end > temp.length/2) {
+                if(temp[end] == '[') {
+                    break;
+                }
+                end--;
+            }
+            if (start >= end)
+                break;
+            
+            swap(temp, start, end);
+            count++;
+            start++;
+            end--;
+        }
+        return count;
+    }
+    
+    private void swap(char[] array, int i, int j) {
+        char temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    
+    private boolean isBalanced(char[] array) {
+        Stack<Character> stack = new Stack<>();
+        for ( char c : array) {
+            if (c == '[')
+                stack.push(c);
+            else {
+                if (stack.isEmpty())
+                    return false;
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
