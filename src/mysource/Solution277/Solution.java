d414a54998bd78cd2c850185b1df4e92bd5c4259
shechiway <shechiway@gmail.com>
2021-10-26 11:36:59 +0800

    [250] 277

--- src/mysource/Solution277/Solution.java
+++ src/mysource/Solution277/Solution.java
@@ -1,6 +1,18 @@
 package mysource.Solution277;
 
 public class Solution {
+    public int findCelebrity_2021_250_faster(int n) {
+        int celebrityIndex = 0;
+        for (int i = 1 ; i < n ; i++) {
+            if (knows(celebrityIndex, i))
+                celebrityIndex = i;
+        }
+        
+        for (int i = 0 ; i < n ; i++) {
+            if (i != celebrityIndex && (knows(celebrityIndex, i) || !knows(i, celebrityIndex))) return -1;
+        }
+        return celebrityIndex;
+    }
     public int findCelebrity_2021_250(int n) {
         int[][] relations = new int[n][n];
         for ( int i = 0 ; i < n ; i++ ) {
