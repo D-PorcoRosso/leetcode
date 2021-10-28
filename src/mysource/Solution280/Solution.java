0d28e78c19477dc1d1356fc07677c6efa999a6b1
shechiway <shechiway@gmail.com>
2021-10-28 12:06:41 +0800

    [250] 280

--- src/mysource/Solution280/Solution.java
+++ src/mysource/Solution280/Solution.java
@@ -1,6 +1,22 @@
 package mysource.Solution280;
 
 public class Solution {
+    public void wiggleSort_2021_250_fast(int[] nums) {
+        for (int i = 1 ; i < nums.length ; i++) {
+            if (i % 2 == 1) {
+                if (nums[i] < nums[i-1])
+                    swap(nums, i, i-1);
+            } else {
+                if (nums[i] > nums[i-1])
+                    swap(nums, i, i-1);
+            }
+        }
+    }
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
     public void wiggleSort_2021_250(int[] nums) {
         Arrays.sort(nums);
         for (int i = 1 ; i < nums.length ; i++) {
