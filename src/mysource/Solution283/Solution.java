1ceab8d22f419bf1a5355f333e7ceaba058774b7
shechiway <shechiway@gmail.com>
2021-10-28 12:06:52 +0800

    [250] 283

--- src/mysource/Solution283/Solution.java
+++ src/mysource/Solution283/Solution.java
@@ -1,6 +1,26 @@
 package mysource.Solution283;
 
 public class Solution {
+    public void moveZeroes_2021_250_fast_alittle(int[] nums) {
+        int i = 0 , j = 0;
+        while(i < nums.length) {
+            while(nums[i] != 0) {
+                i++;
+                if (i == nums.length)
+                    return;
+            }
+            j = i+1;
+            while(j < nums.length && nums[j] == 0) {
+                j++;
+            }
+            if (j == nums.length)
+                return;
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+        }
+    }
     public void moveZeroes_2021_250(int[] nums) {
         for ( int i = nums.length-1 ; i >= 0 ; i--) {
             if (nums[i] == 0) {
