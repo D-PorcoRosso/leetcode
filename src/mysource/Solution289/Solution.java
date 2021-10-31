7c779a333630016a7b11a8f4ddfa93cf8caba075
shechiway <shechiway@gmail.com>
2021-10-31 16:06:55 +0800

    [250] 289

--- src/mysource/Solution289/Solution.java
+++ src/mysource/Solution289/Solution.java
@@ -1,6 +1,63 @@
 package mysource.Solution289;
 
 public class Solution {
+    public void gameOfLife_2021_250_inplace(int[][] board) {
+        for ( int i = 0 ; i < board.length ; i++ ) {
+            for ( int j = 0 ; j < board[0].length ; j++ ) {
+                int liveCount = isLive_2021_250_inplace(board, j, i);
+                if (board[i][j] == 1) {
+                    if (liveCount == 2 || liveCount == 3)
+                        board[i][j] = 1 << 1 | board[i][j];
+                    else
+                        board[i][j] = 1 << 1;
+                } else {
+                    if (liveCount == 3)
+                        board[i][j] = 1;
+                    else
+                        board[i][j] = 0;
+                }
+            }
+        }
+        
+        for ( int i = 0 ; i < board.length ; i++ ) {
+            for ( int j = 0 ; j < board[0].length ; j++ ) {
+                board[i][j] &= 1;
+            }
+        }
+    }
+    
+    private int isLive_2021_250_inplace(int[][] board, int x, int y) {
+        int liveCount = 0;
+        if(x+1 < board[0].length && board[y][x+1] == 1) {
+            liveCount++;
+        }
+        if(y+1 < board.length && board[y+1][x] == 1) {
+            liveCount++;
+        }
+        if(x-1 >= 0 && (board[y][x-1] >> 1) == 1) {
+            liveCount++;
+        }
+        if(y-1 >= 0 && (board[y-1][x] >> 1) == 1) {
+            liveCount++;
+        }
+        
+        
+        if(x+1 < board[0].length && y+1 < board.length && board[y+1][x+1] == 1) {
+            liveCount++;
+        }
+        if(x-1 >= 0 && y-1 >= 0 && (board[y-1][x-1] >> 1) == 1) {
+            liveCount++;
+        }
+        if(x-1 >= 0 && y+1 < board.length && board[y+1][x-1] == 1) {
+            liveCount++;
+        }
+        if(x+1 < board[0].length && y-1 >= 0 && (board[y-1][x+1] >> 1) == 1) {
+            liveCount++;
+        }
+        
+        return liveCount;
+    }
+
     public void gameOfLife_2021_250(int[][] board) {
         int[][] nextBoard = new int[board.length][board[0].length];
         for ( int i = 0 ; i < board.length ; i++ ) {
