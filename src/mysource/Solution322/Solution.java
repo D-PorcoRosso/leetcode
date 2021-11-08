f0b2d96be9f633fdd1fa9cddd3c32a9fb7e7e6cc
shechiway <shechiway@gmail.com>
2021-11-08 11:58:08 +0800

    [250] 322

--- src/mysource/Solution322/Solution.java
+++ src/mysource/Solution322/Solution.java
@@ -1,6 +1,32 @@
 package mysource.Solution322;
 
 public class Solution {
+
+    public int coinChange_2021_250(int[] coins, int amount) {
+        int[] dpTable = new int[amount+1];
+        for ( int i = 0 ; i < dpTable.length ; i++ ) {
+            dpTable[i] = -1;
+        }
+        int result = findMin(coins, dpTable, amount);
+        return result == Integer.MAX_VALUE-1 ? -1 : result;
+    }
+    
+    private int findMin(int[] coins, int[] dpTable, int amount) {
+        int min = Integer.MAX_VALUE-1;
+        if (amount == 0)
+            return 0;
+        if (amount < 0)
+            return min;
+        if(dpTable[amount] != -1)
+            return dpTable[amount];
+        for(int coin : coins) {
+            int current = findMin(coins, dpTable, amount-coin);
+            min = Math.min(min, 1+current);
+        }
+        dpTable[amount] = min;
+        return min;
+    }
+
     public int coinChange(int[] coins, int amount) {
         int[] dpTable = new int[amount+1];
         for ( int i = 0 ; i < dpTable.length ; i++ ) 
