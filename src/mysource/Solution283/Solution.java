a4849484386ebbbb447c14311693d006ab38cce4
shechiway <shechiway@gmail.com>
2021-11-12 18:21:33 +0800

    [Google] 283

--- src/mysource/Solution283/Solution.java
+++ src/mysource/Solution283/Solution.java
@@ -1,6 +1,27 @@
 package mysource.Solution283;
 
 public class Solution {
+    public void moveZeroes_2021_google(int[] nums) {
+        int i = 0, j = 0;
+        while(i < nums.length && j < nums.length) {
+            while(i < nums.length && nums[i] != 0) {
+                i++;
+            }
+            while(j < nums.length && nums[j] == 0) {
+                j++;
+            }
+            if (i < j && j < nums.length && i < nums.length)
+                swap(nums, i, j);
+            j++;
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
     public void moveZeroes_2021_250_fast_alittle(int[] nums) {
         int i = 0 , j = 0;
         while(i < nums.length) {
