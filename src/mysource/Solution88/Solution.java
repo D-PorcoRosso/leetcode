d2a8d10754303fa734476efcedb3f1985303b617
shechiway <shechiway@gmail.com>
2021-11-12 17:35:46 +0800

    [Google] 88

--- src/mysource/Solution88/Solution.java
+++ src/mysource/Solution88/Solution.java
@@ -1,6 +1,20 @@
 package mysource.Solution88;
 
 class Solution {
+
+    public void merge_google(int[] nums1, int m, int[] nums2, int n) {
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for ( int i = 0 ; i < m ; i++ ) {
+            q.offer(nums1[i]);
+        }
+        for ( int i = 0 ; i < n ; i++  ) {
+            q.offer(nums2[i]);
+        }
+        for (int i = 0 ; i < nums1.length ; i++) {
+            nums1[i] = q.poll();
+        }
+    }
+
     public void merge_2021_250_fast(int[] nums1, int m, int[] nums2, int n) {
         if (n == 0) {
             return;
