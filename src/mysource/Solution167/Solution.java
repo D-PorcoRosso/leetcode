2422c3193f8071fdd2c8d04a26ac8ceed8b654de
shechiway <shechiway@gmail.com>
2021-11-12 22:10:51 +0800

    [Google] 167

--- src/mysource/Solution167/Solution.java
+++ src/mysource/Solution167/Solution.java
@@ -1,6 +1,23 @@
 package mysource.Solution167;
 
 class Solution {
+    public int[] twoSum_2021_google_faster(int[] numbers, int target) {
+        int[] result = new int[2];
+        int start = 0, end = numbers.length-1;
+        while(start < end) {
+            int sum = numbers[start] + numbers[end];
+            if (sum > target) {
+                end--;
+            } else if (sum < target)
+                start++;
+            else {
+                result[0] = start+1;
+                result[1] = end+1;
+                return result;
+            }
+        }
+        return result;
+    }
     public int[] twoSum_2021_google(int[] numbers, int target) {
         int[] result = new int[2];
         for ( int i = 0 ; i < numbers.length ; i++ ) {
