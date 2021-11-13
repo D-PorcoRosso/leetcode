4e85e8d44e292b9d4e6809e224600b87c02f3fbf
shechiway <shechiway@gmail.com>
2021-11-13 16:42:59 +0800

    [Google] 557

--- src/mysource/Solution557/Solution.java
+++ src/mysource/Solution557/Solution.java
@@ -4,6 +4,25 @@ import java.util.Stack;
 
 class Solution {
 
+    public String reverseWords_2021_google(String s) {
+        String[] strs = s.split(" ");
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0 ; i < strs.length ; i++) {
+            char[] array = strs[i].toCharArray();
+            for (int j = 0 ; j < array.length/2 ; j++) {
+                char temp = array[j];
+                array[j] = array[array.length-1-j];
+                array[array.length-1-j] = temp;
+            }
+            for (int j = 0 ; j < array.length ; j++) {
+                builder.append(array[j]);
+            }
+            if (i != strs.length-1)
+                builder.append(' ');
+        }
+        return builder.toString();
+    }
+
     public String reverseWords(String s) {
         String result = "";
         Stack<Character> stack = new Stack<>();
