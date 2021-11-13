4ec54b653a5b0670bef7943f3530a80fdad357b1
shechiway <shechiway@gmail.com>
2021-11-13 15:36:27 +0800

    [Google] 442

--- src/mysource/Solution442/Solution.java
+++ src/mysource/Solution442/Solution.java
@@ -1,6 +1,26 @@
 package mysource.Solution442;
 
 public class Solution {
+    public List<Integer> findDuplicates_2021_google_cyclesort(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        int index = 0;
+        while (index < nums.length) {
+            int currentIndex = nums[index]-1;
+            if (nums[index] != nums[currentIndex]) {
+                int temp = nums[currentIndex];
+                nums[currentIndex] = nums[index];
+                nums[index] = temp;
+            } else {
+                index++;
+            }
+        }
+        
+        for(int i = 0 ; i < nums.length ; i++) {
+            if (nums[i] != i+1)
+                result.add(nums[i]);
+        }
+        return result;
+    }
     public List<Integer> findDuplicates_2021_google(int[] nums) {
         List<Integer> result = new ArrayList<>();
         HashSet<Integer> container = new HashSet<>();
