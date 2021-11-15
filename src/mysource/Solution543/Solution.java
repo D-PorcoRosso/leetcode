3f0170c2594dabc85d50ede99a037ccdc93199fb
shechiway <shechiway@gmail.com>
2021-11-15 22:25:16 +0800

    [Google] 543

--- src/mysource/Solution543/Solution.java
+++ src/mysource/Solution543/Solution.java
@@ -10,6 +10,20 @@ class Solution {
 
     private int max = 0;
 
+    public int diameterOfBinaryTree_2021_google(TreeNode root) {
+        findDepth(root);
+        return max;
+    }
+    
+    private int findDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = findDepth(root.left);
+        int right = findDepth(root.right);
+        max = Math.max(max, left+right);
+        return 1+Math.max(left, right);
+    }
+
     public int diameterOfBinaryTree(TreeNode root) {
         maxDepth(root);
         return max;
